c474aaa426222eb186af8ccc27c626dcbe7587f6
David Maluenda Niubó <dmaluenda@cnb.csic.es>
2018-09-11 11:49:26 +0200

    Revert "New defocus estimation with and without phase plate"

--- libraries/reconstruction/ctf_estimate_from_psd_base.cpp
+++ libraries/reconstruction/ctf_estimate_from_psd_base.cpp
@@ -44,7 +44,6 @@ void ProgCTFBasicParams::readBasicParams(XmippProgram *program)
     modelSimplification = program->getIntParam("--model_simplification");
     bootstrap = program->checkParam("--bootstrapFit");
     fastDefocusEstimate = program->checkParam("--fastDefocus");
-    selfEstimation = program->checkParam("--selfEstimation");
     if (fastDefocusEstimate)
     {
         lambdaPhase=program->getDoubleParam("--fastDefocus",0);
@@ -158,8 +157,6 @@ void ProgCTFBasicParams::defineBasicParams(XmippProgram * program)
     program->addParamsLine(
         "   [--enhance_max_freq <f2>]    : Bandpass cutoff. Normalized to 0.5.");
     program->addParamsLine(
-        "   [--selfEstimation]           : Estimate defocus without previous estimation");
-    program->addParamsLine(
         "                                : If fmax>0.35, f2 default=0.08");
     program->addParamsLine(
         "                                : If fmax<0.35, f2 default=0.15");
@@ -182,6 +179,8 @@ void ProgCTFBasicParams::defineParams()
 /* Produce side information ------------------------------------------------ */
 void ProgCTFBasicParams::produceSideInfo()
 {
+    penalty=32.0;
+
     // Resize the frequency
     x_digfreq.initZeros(YSIZE(*f), XSIZE(*f) / 2);
     y_digfreq.initZeros(YSIZE(*f), XSIZE(*f) / 2);
@@ -271,6 +270,7 @@ void ProgCTFBasicParams::produceSideInfo()
 	Filter.generateMask(enhanced_ctftomodel());
 	Filter.applyMaskSpace(enhanced_ctftomodel());
 	STARTINGX(enhanced_ctftomodel()) = STARTINGY(enhanced_ctftomodel()) = 0;
+	//global_prm->enhanced_ctftomodel()= enhanced_ctftomodel();
 
 	// Compute now radial average of the enhanced_ctftomodel
 	psd_exp_enhanced_radial_derivative.initZeros(XSIZE(enhanced_ctftomodel()));
