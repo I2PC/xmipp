12a4051932bb734cbb9d51ff5005cc6956ac691a
dmaluenda <dmaluenda@cnb.csic.es>
2019-01-21 18:01:42 +0100

    Bug fixing on test programs

--- applications/scripts/metadata_selfile_create/batch_metadata_selfile_create.py
+++ applications/scripts/metadata_selfile_create/batch_metadata_selfile_create.py
@@ -27,11 +27,11 @@
 #!/usr/bin/env python
 
 import os
-import xmipp3
+import xmipp_base
 
-class ScriptCreateMetadata(xmipp3.XmippScript):
+class ScriptCreateMetadata(xmipp_base.XmippScript):
     def __init__(self):
-        xmipp3.XmippScript.__init__(self)
+        xmipp_base.XmippScript.__init__(self)
         
     def defineParams(self):
         self.addUsageLine('Create a metadata from a file pattern.')
@@ -50,7 +50,7 @@ class ScriptCreateMetadata(xmipp3.XmippScript):
         pattern = self.getParam('--pattern')
         isStack = self.checkParam('-s')
         label = self.getParam('-l')
-        mD = xmipp3.createMetaDataFromPattern(pattern, isStack, label)                
+        mD = xmipp_base.createMetaDataFromPattern(pattern, isStack, label)
         outFile = self.getParam('-o')
         mD.write(outFile)
 
