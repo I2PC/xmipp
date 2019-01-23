e37bbc1fa1bea793e078d9199d3b9729c080d729
Carlos Oscar S. Sorzano <coss@cnb.csic.es>
2019-01-23 10:45:29 +0100

    xmippLib.* was not working because xmippLib is imported differently

--- bindings/python/xmipp_base.py
+++ bindings/python/xmipp_base.py
@@ -92,10 +92,10 @@ def createMetaDataFromPattern(pattern, isStack=False, label="image"):
     files = glob.glob(pattern)
     files.sort()
 
-    label = xmippLib.str2Label(label) #Check for label value
+    label = str2Label(label) #Check for label value
     
-    mD = xmippLib.MetaData()
-    inFile = xmippLib.FileName()
+    mD = MetaData()
+    inFile = FileName()
     
     nSize = 1
     for file in files:
@@ -103,17 +103,17 @@ def createMetaDataFromPattern(pattern, isStack=False, label="image"):
         if isStack:
             if file.endswith(".mrc"):
                 fileAux=file+":mrcs"
-            x, x, x, nSize = xmippLib.getImageSize(fileAux)
+            x, x, x, nSize = getImageSize(fileAux)
         if nSize != 1:
             counter = 1
             for jj in range(nSize):
                 inFile.compose(counter, fileAux)
                 objId = mD.addObject()
                 mD.setValue(label, inFile, objId)
-                mD.setValue(xmippLib.MDL_ENABLED, 1, objId)
+                mD.setValue(MDL_ENABLED, 1, objId)
                 counter += 1
         else:
             objId = mD.addObject()
             mD.setValue(label, fileAux, objId)
-            mD.setValue(xmippLib.MDL_ENABLED, 1, objId)
+            mD.setValue(MDL_ENABLED, 1, objId)
     return mD            
