6d2f7ef5c92edbd6df2fb9081777060c12ea5364
David Strelak <davidstrelak@gmail.com>
2019-02-01 12:39:45 +0100

    renaming the file + fixing compilation error (missing script)

--- scripts/travis_before_script.py
+++ scripts/travis_before_script.py
@@ -8,9 +8,10 @@ xmipp_folder_name = 'xmipp'
 xmipp_folder = os.path.join(folder, src_folder_name, xmipp_folder_name)
 xmipp_script = 'xmipp'
 sonar_script = 'sonar-project.properties'
+sync_script = 'sync_data.py'
 # get folder relative to home dir and strip os.path separators
 script_dir = os.path.dirname(os.path.realpath(__file__)).split(os.getcwd())[1][1:]
-move_list = [xmipp_script, script_dir, sonar_script]
+copy_list = [xmipp_script, script_dir, sonar_script, sync_script]
 black_list = [src_folder_name]
 os.makedirs(xmipp_folder)
 for item in os.listdir(folder):
@@ -18,11 +19,11 @@ for item in os.listdir(folder):
         continue
     item_path = os.path.join(folder, item)
     try:
-        if item not in move_list:
+        if item not in copy_list:
             shutil.move(item_path, xmipp_folder)
         else:
             if os.path.isdir(item_path):
-                copy_tree(item_path, xmipp_folder)
+                copy_tree(item_path, os.path.join(xmipp_folder, item))
             else:
                 shutil.copy(item_path, xmipp_folder)
     except Exception as e:
