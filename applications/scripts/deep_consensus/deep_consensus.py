80c9b5ad1477bf5ebd22cc1e727fd22edca5f8cc
Ruben Sanchez Garcia <rubensanchezgarc@gmail.com>
2019-02-11 20:26:34 +0100

    auto install cudnn

--- applications/scripts/deep_consensus/deep_consensus.py
+++ applications/scripts/deep_consensus/deep_consensus.py
@@ -32,11 +32,12 @@ import traceback
 
 BAD_IMPORT_MSG='''
 Error, tensorflow/keras is probably not installed. Install it with:\n  ./scipion installb deepLearnigToolkit
-If gpu version of tensorflow desired, install cuda 8.0 and cudnn 6 or cuda 9.0 and cudnn 7 and then
+If gpu version of tensorflow desired, install cuda 8.0 or cuda 9.0
+We will try to automatically install cudnn, if unsucesfully, install cudnn and add to LD_LIBRARY_PATH
 add to SCIPION_DIR/config/scipion.conf
 CUDA = True
 CUDA_VERSION = 8.0  or 9.0
-CUDA_HOME = /path/to/cuda-X
+CUDA_HOME = /path/to/cuda-%(CUDA_VERSION)
 CUDA_BIN = %(CUDA_HOME)s/bin
 CUDA_LIB = %(CUDA_HOME)s/lib64
 CUDNN_VERSION = 6 or 7
