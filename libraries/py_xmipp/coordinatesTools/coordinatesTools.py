b82b5deff7d58e21e994ef105e3534005af5fb04
Ruben Sanchez Garcia <rubensanchezgarc@gmail.com>
2019-02-20 20:33:01 +0100

    fixed deep screening load model

--- libraries/py_xmipp/coordinatesTools/coordinatesTools.py
+++ libraries/py_xmipp/coordinatesTools/coordinatesTools.py
@@ -4,7 +4,7 @@ from xmipp3.convert import readPosCoordinates
 from pyworkflow.em.data import Coordinate, Micrograph
 
 
-def writeCoordsListToPosFname(mic_fname, list_x_y, outputRoot):
+def writeCoordsListToPosFname(mic_fname, list_x_y, outputRoot, micId=None):
 
   s = """# XMIPP_STAR_1 *
 #
@@ -17,6 +17,8 @@ loop_
  _xcoor
  _ycoor
 """
+  if not micId is None:
+    s+=" _micrographId\n"
   baseName= os.path.basename(mic_fname).split(".")[0]
   print("%d %s %s"%(len(list_x_y), mic_fname, os.path.join(outputRoot, baseName+".pos")))
 
@@ -24,11 +26,14 @@ loop_
     with open(os.path.join(outputRoot, baseName+".pos"), "w") as f:
         f.write(s)
         for x, y in list_x_y:
-          f.write(" %d %d\n"%(x,y) )
+          if not micId is None:
+            f.write(" %d %d %d\n"%(x,y, micId) )
+          else:
+            f.write(" %d %d\n"%(x,y) )
 
 
         
-def readPosCoordsFromFName(fname):
+def readPosCoordsFromFName(fname, returnAlsoMicId=False):
   mData= readPosCoordinates(fname)
   coords=[]
   for mdId in mData:
@@ -36,4 +41,8 @@ def readPosCoordsFromFName(fname):
     y=  int( mData.getValue( xmippLib.MDL_YCOOR, mdId) )
     coords.append((x,y) )
   print("N coords: %d"%(len(coords) ))
-  return coords 
+  if returnAlsoMicId:
+    micId= mData.getValue( xmippLib.MDL_MICROGRAPH_ID, mdId)
+    return coords, micId
+  else:
+    return coords 
