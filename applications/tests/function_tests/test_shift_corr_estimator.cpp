e5fcc1e6f0a3c8dc0f9f53a59bd2de603f6ba94c
David Strelak <davidstrelak@gmail.com>
2019-05-02 15:11:59 +0200

    adding shift tests

--- applications/tests/function_tests/test_shift_corr_estimator.cpp
+++ applications/tests/function_tests/test_shift_corr_estimator.cpp
@@ -4,6 +4,9 @@
 template<typename T>
 class AShiftCorrEstimator_Test;
 
+template<typename T>
+class AShiftEstimator_Test;
+
 #define SETUP \
     void SetUp() { \
         estimator = new Alignment::ShiftCorrEstimator<T>(); \
@@ -15,7 +18,14 @@ class AShiftCorrEstimator_Test;
         hw->set(); \
     }
 
+#define INIT \
+    ((Alignment::ShiftCorrEstimator<T>*)estimator)->init2D(*hw, AlignType::OneToN, dims, maxShift, true, true);
+
 #include "ashift_corr_estimator_tests.h"
 
 typedef ::testing::Types<float, double> TestTypes;
 INSTANTIATE_TYPED_TEST_CASE_P(Cpu, AShiftCorrEstimator_Test, TestTypes);
+
+#include "ashift_estimator_tests.h"
+
+INSTANTIATE_TYPED_TEST_CASE_P(ShiftCorrEstimator, AShiftEstimator_Test, TestTypes);
