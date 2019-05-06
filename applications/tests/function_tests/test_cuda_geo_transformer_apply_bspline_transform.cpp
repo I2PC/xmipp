e41afaf3f4b7a6e7192cff289ec3ebb475c6b612
xhorace1 <468922@mail.muni.cz>
2019-05-06 09:42:55 +0200

    Removing unused functions for loading/saving coeffs and output, different precision for float and double

--- applications/tests/function_tests/test_cuda_geo_transformer_apply_bspline_transform.cpp
+++ applications/tests/function_tests/test_cuda_geo_transformer_apply_bspline_transform.cpp
@@ -6,51 +6,34 @@
 
 template< typename T >
 class GeoTransformerTest : public GeoTransformer< T > {
+public:
 
+    void applyBSplineTransformTest(int splineDegree,
+        MultidimArray<T> &output, const MultidimArray<T> &input,
+        const std::pair<Matrix1D<T>, Matrix1D<T>> &coeffs, size_t imageIdx, T outside) {
+        GeoTransformer< T >::applyBSplineTransformRef(splineDegree, output, input, coeffs, imageIdx, outside);
+    }
 };
 
 template< typename T >
 class GeoTransformerApplyBSplineTransformTest : public ::testing::Test {
-
 public:
-
-    /*
-    Version that prints out number of errors
-    */
-    void compare_results( T* true_values, T* approx_values, size_t size ) {
-        testing::internal::CaptureStdout();
-
-        int errors = 0;
+    void compare_results( double* true_values, double* approx_values, size_t size ) {
         for ( int i = 0; i < size; ++i ) {
-            EXPECT_NEAR( true_values[i], approx_values[i], static_cast< T >( 0.0001 ) ) << ( errors++ < 10 ? "at index: " + std::to_string( i ) : "" );
-            if ( errors == 10 ) {
-                std::cout << "Omitting next errors\n";
-            }
-        }
-
-        std::string output = testing::internal::GetCapturedStdout();
-        std::cout << output.substr(0, 100) << std::endl;
-
-        if ( errors > 0 ) {
-            std::cout << "Total errors: " << errors << std::endl;
+            ASSERT_NEAR( true_values[i], approx_values[i], 0.000001 ) << "at index:" << i;
         }
     }
 
     /*
-
+     * For big matrices there can be a small number of pixels whose value differs more than 0.0001
+     * from reference values, therefore I test it for lower precision with floats
+     * If tested with double values we require high precision
     */
-    // void compare_results( T* true_values, T* approx_values, size_t size ) {
-        // for ( int i = 0; i < size; ++i ) {
-            // EXPECT_NEAR( true_values[i], approx_values[i], static_cast< T >( 0.00001 ) ) << "at index:" << i << ", x=" << i % x << ", y=" << i / x;
-        // }
-    // }
-
-
-    // void compare_results( T* true_values, T* approx_values, size_t size ) {
-    //     for ( int i = 0; i < size; ++i ) {
-    //         ASSERT_NEAR( true_values[i], approx_values[i], static_cast< T >( 0.0001 ) ) << "at index:" << i;
-    //     }
-    // }
+    void compare_results( float* true_values, float* approx_values, size_t size ) {
+        for ( int i = 0; i < size; ++i ) {
+            ASSERT_NEAR( true_values[i], approx_values[i], 0.001f ) << "at index:" << i;
+        }
+    }
 
     void allocate_arrays() {
         size = x * y;
@@ -66,13 +49,13 @@ public:
     void run_transformation() {
         GeoTransformerTest< T > gt;
         gt.initLazyForBSpline( x, y, 1, splineX, splineY, splineN );
-        gt.applyBSplineTransformNew( 3, out, in, { coeffsX, coeffsY }, imageIdx, outside );
+        gt.applyBSplineTransform( 3, out, in, { coeffsX, coeffsY }, imageIdx, outside );
     }
 
     void compute_reference_result() {
         GeoTransformerTest< T > gt;
         gt.initLazyForBSpline( x, y, 1, splineX, splineY, splineN );
-        gt.applyBSplineTransform( 3, out_ref, in, { coeffsX, coeffsY }, imageIdx, outside );
+        gt.applyBSplineTransformTest( 3, out_ref, in, { coeffsX, coeffsY }, imageIdx, outside );
     }
 
     std::pair< size_t, size_t > random_size( int seed ) {
@@ -105,58 +88,6 @@ public:
             MATRIX1D_ARRAY( array )[i] = dis(gen);
         }
 
-    }
-
-    void save_array( const MultidimArray< T >& array, const std::string& filename ) {
-    	std::ofstream out( filename );
-
-    	// out << std::setprecision( 7 );
-    	out << array;
-    }
-
-    void save_coeffs( const Matrix1D< T >& array, const std::string& filename ) {
-    	std::ofstream out( filename );
-
-    	// out << std::setprecision( 7 );
-    	out << array;
-    }
-
-    MultidimArray< T > load_array( const std::string& filename ) {
-    	std::ifstream input( filename );
-
-    	MultidimArray< T > array;
-    	array.resize( y, x );
-
-    	size_t index = 0;
-
-    	while ( true ) {
-    		T value;
-    		input >> value;
-    		if ( input.eof() ) break;
-    		DIRECT_MULTIDIM_ELEM( array, index++ ) = value;
-    	}
-    	assert( index == y * x );
-
-    	return array;
-    }
-
-    Matrix1D< T > load_coeffs( const std::string& filename ) {
-    	std::ifstream input( filename );
-
-    	Matrix1D< T > coeffs;
-    	coeffs.resize( splineX * splineY * splineN );
-
-    	size_t index = 0;
-
-    	while ( true ) {
-    		T value;
-    		input >> value;
-    		if ( input.eof() ) break;
-    			MATRIX1D_ARRAY( coeffs )[index++] = value;
-    	}
-    	assert( index == splineX * splineY * splineN );
-
-    	return coeffs;
     }
 
     size_t x;
