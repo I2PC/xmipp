12f66e957e3c8d0eff2fe08d199e751229118f26
rsanchezgarc <rubensanchezgarc@gmail.com>
2019-09-06 11:37:08 -0400

    Fix deep consensus dot (#191)
    * compatibility with dot_files
    
    * Bug fix, because list does not have split
    
    * fixed bug dot coordinates
    
    * fixed dot in extract_particles
    
    * pwutils independet
    
    * fix slicing bug
    
    * prevent tf being stuck new model
    
    * k.clear_session cud9

--- src/xmipp/applications/scripts/extract_particles/extract_particles.py
+++ src/xmipp/applications/scripts/extract_particles/extract_particles.py
@@ -29,7 +29,6 @@ import sys, os
 from joblib import delayed, Parallel
 from subprocess import check_call
 
-import numpy as np
 import xmipp_base
 
 class ScriptExtractParticles(xmipp_base.XmippScript):
@@ -84,7 +83,8 @@ class ScriptExtractParticles(xmipp_base.XmippScript):
               raise ValueError("Error, input file bad format. If -c option, it must have 2 cols: 'micFname coordsFname'")
             else:
               mic_fname= lineArray[0]
-              mic_basename=  os.path.basename(mic_fname).split(".")[0]
+              mic_basename_split=  os.path.basename(mic_fname).split(".")
+              mic_basename= ".".join(mic_basename_split[:-1]) if len(mic_basename_split)>1 else mic_basename_split[0]
               out_name= os.path.join(outDir, mic_basename)
               pos_fname= lineArray[1]
               if not pos_fname.startswith("particles@"):
