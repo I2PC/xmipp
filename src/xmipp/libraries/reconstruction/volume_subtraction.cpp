4be397c40e00fdf92903a8adcde60cd0e2515367
estrellafg <estrellafergi@gmail.com>
2020-06-11 16:20:03 +0200

    remove pdb flag, add sub flag, remove saving intermediate results

--- src/xmipp/libraries/reconstruction/volume_subtraction.cpp
+++ src/xmipp/libraries/reconstruction/volume_subtraction.cpp
@@ -65,20 +65,22 @@ class ProgVolumeSubtraction: public XmippProgram
 {
 protected:
 	FileName fnVol1, fnVol2, fnDiff, fnMask1, fnMask2;
-	bool pdb;
+//	bool pdb;
+	bool sub;
 	int iter, sigma;
 	double cutFreq;
 
     void defineParams()
     {
         //Usage
-        addUsageLine("Calculate the subtraction of two volumes");
+        addUsageLine("This program scales a volume in order to assimilate it to another one. Then, it can calculate the subtraction of the two volumes.");
         //Parameters
-        addParamsLine("-i1 <volume>          	: First volume to subtract");
-        addParamsLine("-i2 <volume>          	: Second volume to subtract");
-        addParamsLine("[-o <structure=\"\">] 	: Volume difference");
-        addParamsLine("                      	: If no name is given, then volume_diff.mrc");
-        addParamsLine("[--pdb]    			 	: Second volume come from a pdb");
+        addParamsLine("-i1 <volume>          	: Reference volume");
+        addParamsLine("-i2 <volume>          	: Volume to modify");
+        addParamsLine("[-o <structure=\"\">] 	: Volume 2 modified or volume difference");
+        addParamsLine("                      	: If no name is given, then output_volume.mrc");
+//        addParamsLine("[--pdb]    			 	: Second volume come from a pdb");
+        addParamsLine("[--sub] 			        : Perform the subtraction of the volumes. Output will be the difference");
         addParamsLine("[--sigma <s=3>]    		: Decay of the filter (sigma) to smooth the mask transition");
         addParamsLine("[--iter <n=1>]        	: Number of iterations");
         addParamsLine("[--mask1 <mask=\"\">]  	: Mask for volume 1");
@@ -92,8 +94,9 @@ protected:
     	fnVol2=getParam("-i2");
     	fnDiff=getParam("-o");
     	if (fnDiff=="")
-    		fnDiff="volume_diff.mrc";
-    	pdb=checkParam("--pdb");
+    		fnDiff="output_volume.mrc";
+//    	pdb=checkParam("--pdb");
+    	sub=checkParam("--sub");
     	iter=getIntParam("--iter");
     	sigma=getIntParam("--sigma");
     	fnMask1=getParam("--mask1");
@@ -141,16 +144,16 @@ protected:
             mask().initConstant(1.0);
 		}
     	mask1.clear();
-		mask.write("commonmask.mrc");
+//		mask.write("commonmask.mrc");
 		POCSmask(mask(),V());
     	POCSnonnegative(V());
     	transformer.FourierTransform(V(),V1Fourier,false);
     	FFT_magnitude(V1Fourier,V1FourierMag);
 		double std1 = V().computeStddev();
-		V.write("V1masked.mrc");
+//		V.write("V1masked.mrc");
 		V.read(fnVol2);
 		POCSmask(mask(),V());
-		V.write("V2masked.mrc");
+//		V.write("V2masked.mrc");
 
     	MultidimArray<std::complex<double> > V2FourierPhase;
     	transformer.FourierTransform(V(),V2FourierPhase,true);
@@ -162,39 +165,36 @@ protected:
     		POCSFourierAmplitude(V1FourierMag,V2Fourier);
         	transformer.inverseFourierTransform();
 			POCSmask(mask(),V());
-    		V.write(formatString("V2masked_Amp1_%d.mrc", n));
+//    		V.write(formatString("V2masked_Amp1_%d.mrc", n));
     		transformer.FourierTransform();
     		//V2FourierPhase = V2Fourier;
         	//extractPhase(V2FourierPhase);
     		POCSFourierPhase(V2FourierPhase,V2Fourier);
         	transformer.inverseFourierTransform();
-    		V.write(formatString("V2masked_Amp1_ph2_%d.mrc", n));
+//    		V.write(formatString("V2masked_Amp1_ph2_%d.mrc", n));
         	POCSnonnegative(V());
 			double  std2 = V().computeStddev();
 			V()*=std1/std2;
-    		V.write(formatString("V2masked_Amp1_ph2_nonneg_%d.mrc", n));
+//    		V.write(formatString("V2masked_Amp1_ph2_nonneg_%d.mrc", n));
     	}
 
-    	Image<double> V1, V1Filtered;
-    	V1.read(fnVol1);
-
-    	V1() -= V();
-    	V1.write("subtraction.mrc");
-
-    	// Filter common mask with gaussian for smoothing
-        FourierFilter Filter;
-        Filter.FilterShape=REALGAUSSIAN;
-        Filter.FilterBand=LOWPASS;
-        Filter.w1=sigma;
-    	Filter.applyMaskSpace(mask());
-    	mask.write("maskfilter.mrc");
-
-    	V1.read(fnVol1);
+//		Image<double> V1, V1Filtered;
+//    	V1.read(fnVol1);
+//    	V1() -= V();
+//    	V1.write("subtraction.mrc");
+		// Filter common mask with gaussian for smoothing
+		FourierFilter Filter;
+		Filter.FilterShape=REALGAUSSIAN;
+		Filter.FilterBand=LOWPASS;
+		Filter.w1=sigma;
+		Filter.applyMaskSpace(mask());
+//		mask.write("maskfilter.mrc");
+		Image<double> V1, V1Filtered;
+		V1.read(fnVol1);
 		V1Filtered() = V1();
-
-    	// If cutoff freq param is passed, filter V1 and V2
-    	if (cutFreq!=0)
-    	{
+		// If cutoff freq param is passed, filter V1 and V2
+		if (cutFreq!=0)
+		{
 			FourierFilter Filter2;
 			Filter2.FilterBand=LOWPASS;
 			Filter2.FilterShape=RAISED_COSINE;
@@ -203,16 +203,20 @@ protected:
 			Filter2.generateMask(V());
 			Filter2.do_generate_3dmask=true;
 			Filter2.applyMaskSpace(V());
-			V.write("V2filter.mrc");
+//			V.write("V2filter.mrc");
 			Filter2.applyMaskSpace(V1Filtered());
-			V1Filtered.write("V1filter.mrc");
+//			V1Filtered.write("V1filter.mrc");
+		}
+
+    	if (sub==true)
+    	{
+    		FOR_ALL_DIRECT_ELEMENTS_IN_MULTIDIMARRAY(V1())
+    		DIRECT_MULTIDIM_ELEM(V1,n) = DIRECT_MULTIDIM_ELEM(V1,n)*(1-DIRECT_MULTIDIM_ELEM(mask,n)) + (DIRECT_MULTIDIM_ELEM(V1Filtered, n) -
+    				std::min(DIRECT_MULTIDIM_ELEM(V,n), DIRECT_MULTIDIM_ELEM(V1Filtered, n)))*DIRECT_MULTIDIM_ELEM(mask,n);
+    		V1.write(fnDiff);
     	}
 
-
-		FOR_ALL_DIRECT_ELEMENTS_IN_MULTIDIMARRAY(V1())
-		DIRECT_MULTIDIM_ELEM(V1,n) = DIRECT_MULTIDIM_ELEM(V1,n)*(1-DIRECT_MULTIDIM_ELEM(mask,n)) + (DIRECT_MULTIDIM_ELEM(V1Filtered, n) -
-				std::min(DIRECT_MULTIDIM_ELEM(V,n), DIRECT_MULTIDIM_ELEM(V1Filtered, n)))*DIRECT_MULTIDIM_ELEM(mask,n);
-
-		V1.write(fnDiff);
+    	else
+    		V.write(fnDiff);
     }
 };
