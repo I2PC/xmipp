67873035e8e2baa787ba7a7b3e5ab6c06779b37d
David Strelak <davidstrelak@gmail.com>
2020-10-20 09:52:55 +0200

    Updating: CUDA: 8.0 -> 9.2 OS: Trusty -> Xenial https://docs.travis-ci.com/user/reference/xenial/

--- scripts/install_cuda_travis.sh
+++ scripts/install_cuda_travis.sh
@@ -10,9 +10,9 @@
 # 'source' this script, rather than executing it in a sub-process.
 #
 
-travis_retry wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_${CUDA_VER}_amd64.deb
+travis_retry wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_${CUDA_VER}_amd64.deb
 travis_retry sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
-travis_retry sudo dpkg -i cuda-repo-ubuntu1404_${CUDA_VER}_amd64.deb
+travis_retry sudo dpkg -i cuda-repo-ubuntu1604_${CUDA_VER}_amd64.deb
 travis_retry sudo apt-get update -qq
 export CUDA_VER_TMP=$(expr ${CUDA_VER} : '\([0-9]*\.[0-9]*\)')
 export CUDA_APT=${CUDA_VER_TMP/./-}
