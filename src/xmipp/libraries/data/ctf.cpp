6fb88831ee3adf4d9d458700ffb989620522d474
Jan Horacek <xhorace4@fi.muni.cz>
2021-04-17 10:13:50 +0200

    Minor code style enhancement.

--- src/xmipp/libraries/data/ctf.cpp
+++ src/xmipp/libraries/data/ctf.cpp
@@ -1204,7 +1204,7 @@ void CTFDescription::readFromMdRow(const MDRow &row, bool disable_if_not_K)
 
 void CTFDescription::readFromMetadataRow(const MetaData &md, size_t id, bool disable_if_not_K)
 {
-	std::unique_ptr<const MDRow> row = std::move(md.getRow(id));
+	std::unique_ptr<const MDRow> row = md.getRow(id);
 	readFromMdRow(*row, disable_if_not_K);
 }
 
