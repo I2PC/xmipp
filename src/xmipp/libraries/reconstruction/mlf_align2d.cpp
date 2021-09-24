4c3ee6b351564ae1ffc0d1af39f382da4313397f
DavidHerreros <david.herreroscalero@gmail.com>
2021-09-24 16:37:22 +0200

    Intialize restart_seed variable

--- src/xmipp/libraries/reconstruction/mlf_align2d.cpp
+++ src/xmipp/libraries/reconstruction/mlf_align2d.cpp
@@ -98,6 +98,7 @@ void ProgMLF2D::readParams()
     double restart_offset = 0;
     FileName restart_imgmd, restart_refmd;
     int restart_iter, restart_seed;
+    restart_seed = 0;
 
     do_restart = checkParam("--restart");
     if (do_restart)
