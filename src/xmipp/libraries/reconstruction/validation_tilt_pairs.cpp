f796fa1b08ba87d1691d1fbfc0db27fc33e70b26
EstrellaFG <estrellafergi@gmail.com>
2021-09-24 13:24:39 +0200

    initialize alpha_x and alpha_y

--- src/xmipp/libraries/reconstruction/validation_tilt_pairs.cpp
+++ src/xmipp/libraries/reconstruction/validation_tilt_pairs.cpp
@@ -301,6 +301,8 @@ void ProgValidationTiltPairs::run()
 					double tiltt=YY(anglesj);
 					double psit=ZZ(anglesj);
 					double alpha_x, alpha_y;
+					alpha_x = 0;
+					alpha_y = 0;
 					Euler_angles2matrix(rott,tiltt,psit,Et,false);
 					//////////////////////////////////////////////////////////////////
 					double untilt_angles[3]={rotu, tiltu, psiu}, tilt_angles[3]={rott, tiltt, psit};
