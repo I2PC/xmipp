57c9b8e9a963bd375f952ef707af896fb99abb0a
Vilax <jlvilasprieto@gmail.com>
2021-09-24 16:30:53 +0200

    ptrnull fixes

--- src/xmipp/libraries/reconstruction/program_extension.cpp
+++ src/xmipp/libraries/reconstruction/program_extension.cpp
@@ -50,7 +50,7 @@ void runSystem(const String &program, const String &arguments, bool useSystem) {
 
 int runProgram(XmippProgram * program, const String &arguments, bool destroy)
 {
-    if (program == NULL)
+    if (program == nullptr)
         REPORT_ERROR(ERR_PARAM_INCORRECT, "Received a NULL as program pointer");
     program->read(arguments);
     int retCode = program->tryRun();
@@ -100,7 +100,7 @@ XmippProgram * getProgramByName(const String &programName)
     if (programName == "xmipp_transform_filter")
         return new ProgFilter();
 
-    return NULL;
+    return nullptr;
 }
 
 
