976709694a1576bdfba2c10fd25b3560fd6827ac
Carlos Oscar Sorzano <coss@cnb.csic.es>
2022-01-28 12:44:39 +0100

    Removal of sonarcloud issues

--- src/xmipp/libraries/data/polar.h
+++ src/xmipp/libraries/data/polar.h
@@ -555,7 +555,7 @@ public:
     void getCartesianCoordinates(std::vector<double> &x,
                                  std::vector<double> &y,
                                  std::vector<T> &data,
-                                 const double extra_shell = GRIDDING_K/2)
+                                 const int extra_shell = GRIDDING_K/2)
     {
         // Only for full circles for now!
         if (mode != FULL_CIRCLES)
@@ -582,15 +582,16 @@ public:
         // Add additional points on the inside and outside of the rings
         // Add a maximum of "extra_shell" rings
         // Set data to zero here
-        double first_ring  = ring_radius[0];
-        double last_ring   = ring_radius[rings.size()-1];
-        float outer       = last_ring + extra_shell;
-        float inner       = XMIPP_MAX(0.,first_ring - extra_shell);
-        for (float radius = 0.; radius < outer; radius +=1.)
+        int first_ring  = (int)floor(ring_radius[0]);
+        int last_ring   = (int)ceil(ring_radius[rings.size()-1]);
+        int outer       = last_ring + extra_shell;
+        int inner       = XMIPP_MAX(0,first_ring - extra_shell);
+        for (int iradius = 0; iradius < outer; iradius +=1)
         {
-            if ( (radius >= inner && radius < first_ring) ||
-                 ( radius <= outer && radius > last_ring) )
+            if ( (iradius >= inner && iradius < first_ring) ||
+                 (iradius <= outer && iradius > last_ring) )
             {
+            	double radius=iradius;
                 int nsam = 2 * (int)( 0.5 * oversample * TWOPI * radius );
                 nsam = XMIPP_MAX(1, nsam);
                 float dphi = TWOPI / (float)nsam;
@@ -603,7 +604,6 @@ public:
                 }
             }
         }
-
     }
 
     /** Convert cartesian MultidimArray to Polar using B-spline interpolation
