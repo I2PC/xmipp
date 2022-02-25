26c11afc8fe3d1c049264ddac80e554e9e436803
fede-pe <fepe.ig@gmail.com>
2022-02-25 13:34:30 +0100

    fix src/xmipp/libraries/reconstruction/volume_halves_restoration.cpp

--- src/xmipp/libraries/reconstruction/volume_halves_restoration.cpp
+++ src/xmipp/libraries/reconstruction/volume_halves_restoration.cpp
@@ -373,8 +373,14 @@ void ProgVolumeHalvesRestoration::filterBank()
 	int imax=ceil(0.5/filterStep);
 	std::cerr << "Calculating filter bank ..." << std::endl;
 	init_progress_bar(imax);
-	for (double w=0; w<0.5; w+=filterStep)
-	{
+
+	double w;
+	size_t totalSteps = (int)(0.5/filterStep);
+
+	for (size_t nSteps = 0; nSteps < totalSteps; totalSteps++)
+	{	
+		w = nSteps*filterStep;
+	
 		filterBand(fV1r,bank1,w);
 		filterBand(fV2r,bank2,w);
 
