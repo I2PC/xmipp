650966b327f0b574a16a315de01c9f860c673082
albertmena <alber.mena@gmail.com>
2022-03-03 13:46:41 +0100

    test to read MRC done

--- src/xmipp/applications/tests/function_tests/test_rwMRC.cpp
+++ src/xmipp/applications/tests/function_tests/test_rwMRC.cpp
@@ -1,10 +1,9 @@
-#include <core/rwMRC.h>
 #include <iostream>
 #include <gtest/gtest.h>
 #include <core/xmipp_image_base.h>
+#include <core/xmipp_image_extension.h>
 #include <core/xmipp_image.h>
 #include <filesystem>// MORE INFO HERE: http://code.google.com/p/googletest/wiki/AdvancedGuide
-
 
 class rwMRCTest : public ::testing::Test {
 protected:
@@ -14,37 +13,22 @@ protected:
         path+="/xmipp/resources/test";
         if (chdir(path.c_str()) != 0 ) FAIL() << "Could not change path to: " << path;
         imageName = "image/singleImage.mrc";
-//        try
-//        {
-//            im.read(imageName);
-//        }
-//        catch (XmippError &xe)
-//        {
-//            std::cerr << xe;
-//            exit(-1);
-//        }
     }
     //Image to be fitted:
     Image<double> im;
-    //File name of the image to process
     FileName imageName;
 };
 
 
-TEST_F(rwMRCTest, readMapped) {
-//    int datamode = 2;
-      size_t select_img = sizeof(imageName)
-//    bool mapData = False;
-//    int mode = 2;
-    ASSERT_EQ(im.readMapped(imageName, select_img), 0);
-    //ASSERT_EQ(im.read(imageName, datamode, select_img, mapData, mode), 0);
-    //err = ImageBase::readMRC(imageName,false); is protected
-    //ImageBase::dataMode
+TEST_F(rwMRCTest, readMRC) {
+    //ASSERT_TRUE(im.read(imageName, 3, select_img, false, datamode) ==0);
+    ASSERT_TRUE(im.read(imageName) == 0);
 }
 
+
 //TEST_F(rwMRCTest, writeMRC) {
-//    while (file has still elements) {
-//      wchar_t* expectedField = L"expectingThis";
-//      wchar_t* readString = el->field;
-//      EXPECT_EQ(expectedField,writeMRC);
-//    }
+//    im2w.setDataMode(_DATA_ALL);
+//    ASSERT_TRUE(im2w.write(imageNameWrite) == 0);
+//    im2w.clear();
+//    ASSERT_TRUE(im2w.read(imageName) == 0);
+//}
