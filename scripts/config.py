37affca727c44bc5d4da618446ebf8d89f71790a
albertmena <12760268+albertmena@users.noreply.github.com>
2022-05-13 10:24:08 +0200

    bug fied install-deps: OFF trying to reduce time spent installing

--- scripts/config.py
+++ scripts/config.py
@@ -172,7 +172,7 @@ class Config:
         labels = [Config.KEY_BUILD_TESTS, 'CC', 'CXX', 'LINKERFORPROGRAMS', 'INCDIRFLAGS', 'LIBDIRFLAGS', 'CCFLAGS', 'CXXFLAGS',
                   'LINKFLAGS', 'PYTHONINCFLAGS', 'MPI_CC', 'MPI_CXX', 'MPI_RUN', 'MPI_LINKERFORPROGRAMS', 'MPI_CXXFLAGS',
                   'MPI_LINKFLAGS', 'NVCC', 'CXX_CUDA', 'NVCC_CXXFLAGS', 'NVCC_LINKFLAGS',
-                  'MATLAB_DIR', 'CUDA', 'DEBUG', 'MATLAB', 'OPENCV_VERSION', 'OPENCVSUPPORTSCUDA',
+                  'MATLAB_DIR', 'CUDA', 'DEBUG', 'MATLAB', 'OPENCV', 'OPENCVSUPPORTSCUDA', 'OPENCV_VERSION',
                   'JAVA_HOME', 'JAVA_BINDIR', 'JAVAC', 'JAR', 'JNI_CPPPATH',
                   'STARPU', 'STARPU_HOME', 'STARPU_INCLUDE', 'STARPU_LIB', 'STARPU_LIBRARY',
                   'USE_DL', 'VERIFIED', 'CONFIG_VERSION', 'PYTHON_LIB']
@@ -194,10 +194,9 @@ class Config:
             print(yellow("OpenCV not found"))
             self.configDict["OPENCV"] = False
             self.configDict["OPENCVSUPPORTSCUDA"] = False
-            self.configDict["OPENCV_VERSION"] = False
+            self.configDict["OPENCV_VERSION"] = ''
         else:
             self.configDict["OPENCV"] = True
-
             # Check version
             with open("xmipp_test_opencv.cpp", "w") as cppFile:
                 cppFile.write('#include <opencv2/core/version.hpp>\n')
@@ -211,8 +210,7 @@ class Config:
             if not runJob("%s -w %s xmipp_test_opencv.cpp -o xmipp_test_opencv %s "
                           % (self.get(Config.KEY_CXX), self.configDict["CXXFLAGS"],
                              self.configDict["INCDIRFLAGS"]), show_output=False):
-                self.configDict["OPENCV_VERSION"] = False
-                version = 2  # Just in case
+                self.configDict["OPENCV_VERSION"] = 2
             else:
                 runJob("./xmipp_test_opencv")
                 f = open("xmipp_test_opencv.txt")
@@ -378,7 +376,7 @@ class Config:
                 ["-I%s" % iDir for iDir in incDirs])
 
         self.configDict["OPENCV"] = os.environ.get("OPENCV", "")
-        if self.configDict["OPENCV"] == "" or self.configDict["OPENCVSUPPORTSCUDA"] or self.configDict["OPENCV_VERSION"]:
+        if self.configDict["OPENCV"] == "" or self.configDict["OPENCVSUPPORTSCUDA"]:
             self._config_OpenCV()
 
     def _get_GCC_version(self, compiler):
