1e2e137294026f56f302b19f3c64334328e27bf2
alberto <12760268+albertmena@users.noreply.github.com>
2022-05-24 15:27:58 +0200

    Update xmipp_conda_envs.py

--- src/xmipp/bindings/python/xmipp_conda_envs.py
+++ src/xmipp/bindings/python/xmipp_conda_envs.py
@@ -26,16 +26,15 @@ XMIPP_CONDA_ENVS = {
     "pipPackages": [],
     "defaultInstallOptions": {},
     "xmippEnviron": False
-  }
-  #,
-#   "xmipp_deepHand": {
-#     "pythonVersion": "3.8",
-#     "dependencies": ["pytorch=1.6"],
-#     "channels": ["anaconda", "conda-forge"],
-#     "pipPackages": [],
-#     "defaultInstallOptions": {},
-#     "xmippEnviron": False
-# }
+  },
+  "xmipp_deepHand": {
+    "pythonVersion": "3.6",
+    "dependencies": ["pytorch=1.6"],
+    "channels": ["anaconda", "conda-forge"],
+    "pipPackages": [],
+    "defaultInstallOptions": {},
+    "xmippEnviron": False
+}
 
 
 }
