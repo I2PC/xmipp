6ed3487b2a9ee29e84b5bb6ef973082820725d8f
Jakub Kiska <kubo.kiska@gmail.com>
2022-09-22 13:41:08 +0200

    Revert "Revert "Iterate through Z in backward""
    This reverts commit 4ee88bd122a4d06672a789ec722b77a517081dcb.

--- src/xmipp/libraries/reconstruction_cuda/cuda_forward_art_zernike3d.cpp
+++ src/xmipp/libraries/reconstruction_cuda/cuda_forward_art_zernike3d.cpp
@@ -256,7 +256,7 @@ Program<PrecisionType>::Program(const Program<PrecisionType>::ConstantParameters
 	  gridXStep(parameters.Vrefined().xdim / loopStep / blockXStep),
 	  gridYStep(parameters.Vrefined().ydim / loopStep / blockYStep),
 	  gridZStep(parameters.Vrefined().zdim / loopStep / blockZStep),
-	  xdimB(parameters.VRecMaskB.xdim),
+	  zdimB(parameters.VRecMaskB.xdim),
 	  ydimB(parameters.VRecMaskB.ydim)
 {
 	//cudaCoordinatesF = filterAndTransportMask(parameters.VRecMaskF);
@@ -364,7 +364,7 @@ void Program<PrecisionType>::runBackwardKernel(struct DynamicParameters &paramet
 	backwardKernel<PrecisionType, usesZernike><<<gridX, blockX>>>(cudaMV,
 																  cudaMId,
 																  cudaCoordinatesB,
-																  xdimB,
+																  zdimB,
 																  ydimB,
 																  static_cast<unsigned>(sizeB),
 																  lastZ,
