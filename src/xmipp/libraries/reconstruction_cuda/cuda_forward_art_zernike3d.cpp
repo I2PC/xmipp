27b77d300c3ab93395a7427c981e2614f71404a8
Jakub Kiska <kubo.kiska@gmail.com>
2022-10-11 10:43:14 +0200

    Add debug info

--- src/xmipp/libraries/reconstruction_cuda/cuda_forward_art_zernike3d.cpp
+++ src/xmipp/libraries/reconstruction_cuda/cuda_forward_art_zernike3d.cpp
@@ -12,6 +12,8 @@
 #include <utility>
 #include "data/numerical_tools.h"
 
+#include <cstdio>
+
 namespace cuda_forward_art_zernike3D {
 
 // Cuda memory helper function
@@ -379,21 +381,29 @@ void Program<PrecisionType>::runForwardKernel(struct DynamicParameters &paramete
 
 	cudaDeviceSynchronize();
 
+	printf("1\n");
+
 	updateVectorOfMultidimArrayWithGPUData(parameters.P, pVector);
 	updateVectorOfMultidimArrayWithGPUData(parameters.W, wVector);
 
+	printf("2\n");
+
 	freeVectorOfMultidimArray(pVector);
 	freeVectorOfMultidimArray(wVector);
+	printf("3\n");
 	cudaFree(cudaP);
 	cudaFree(cudaW);
 	cudaFree(cudaSigma);
+	printf("4\n");
 	freeCommonArgumentsKernel<PrecisionType>(commonParameters);
+	printf("5\n");
 }
 
 template<typename PrecisionType>
 template<bool usesZernike>
 void Program<PrecisionType>::runBackwardKernel(struct DynamicParameters &parameters)
 {
+	printf("6\n");
 	// Unique parameters
 	auto &mId = parameters.Idiff();
 	auto cudaMId = initializeMultidimArrayCuda(mId);
