2c3e44056927ae5593deab5106e2bfcf17c183d7
Jakub Kiska <kubo.kiska@gmail.com>
2022-11-14 01:30:09 +0100

    Test speed without atomic add

--- src/xmipp/libraries/reconstruction_cuda/cuda_forward_art_zernike3d.cu
+++ src/xmipp/libraries/reconstruction_cuda/cuda_forward_art_zernike3d.cu
@@ -315,8 +315,10 @@ namespace device {
 		int i = static_cast<int>(CUDA_ROUND(pos_y));
 		int j = static_cast<int>(CUDA_ROUND(pos_x));
 		if (!IS_OUTSIDE2D(mP, i, j)) {
-			atomicAddPrecision(&A2D_ELEM(mP, i, j), weight);
-			atomicAddPrecision(&A2D_ELEM(mW, i, j), CST(1.0));
+			//atomicAddPrecision(&A2D_ELEM(mP, i, j), weight);
+			//atomicAddPrecision(&A2D_ELEM(mW, i, j), CST(1.0));
+			A2D_ELEM(mP, i, j) += weight;
+			A2D_ELEM(mW, i, j) += CST(1.0);
 		}
 	}
 
