0cd40e40b6af47975ded3cdf4724c2807aa0b59b
albertmena <12760268+albertmena@users.noreply.github.com>
2022-12-28 11:01:39 +0100

    Fixing deprecating: xray_project

--- src/xmipp/libraries/data/psf_xr.h
+++ src/xmipp/libraries/data/psf_xr.h
@@ -133,8 +133,7 @@ public:
     Matrix2D<double>  T;
 
     /// Lens shape Mask
-    MultidimArray<double> *mask; //TODO: As in threadXrayProject a copy of this class is done, this pointer is never deleted. This class should use
-    // applyOTF simultaneously from several threads
+    MultidimArray<double> *mask; // This class should use applyOTF simultaneously from several threads
 
     /* RX Microscope configuration */
     /// Lens Aperture Radius
