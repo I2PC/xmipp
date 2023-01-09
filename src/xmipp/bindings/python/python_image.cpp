afb7cf2e4f759eec1577d629ee8257ea6493936b
David Strelak <davidstrelak@gmail.com>
2023-01-09 13:32:53 +0100

    adding executable for the PSD estimator adding normalization (default for binary, skipped for Python to preserve backward compatibility)

--- src/xmipp/bindings/python/python_image.cpp
+++ src/xmipp/bindings/python/python_image.cpp
@@ -1255,7 +1255,7 @@ Image_computePSD(PyObject *obj, PyObject *args, PyObject *kwargs)
             MultidimArray<double> *out;
             MULTIDIM_ARRAY_GENERIC(*result->image).getMultidimArrayPointer(out);
             // call the estimation
-            PSDEstimator<double>::estimatePSD(*in, overlap, dims, *out, threads);
+            PSDEstimator<double>::estimatePSD(*in, overlap, dims, *out, threads, false);
         } else {
             PyErr_SetString(PyXmippError, "Unknown error while allocating data for output or parsing data");
         }
