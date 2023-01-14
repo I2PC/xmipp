3488b6ad70ffb8b93d6f32ac1f6d5e842f5cf577
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2023-01-14 10:55:12 +0100

    Removed extra parenthesis

--- src/xmipp/applications/scripts/torch/alignment/generate_alignment_metadata.py
+++ src/xmipp/applications/scripts/torch/alignment/generate_alignment_metadata.py
@@ -69,7 +69,7 @@ def generate_alignment_metadata(experimental_md: pd.DataFrame,
     })
     
     # Use the first match
-    alignment_md = _ensemble_alignment_md(reference_md, projection_md, match_indices[:,0], match_distances[:,0]))
+    alignment_md = _ensemble_alignment_md(reference_md, projection_md, match_indices[:,0], match_distances[:,0])
     
     # Add the alignment consensus to the output
     output_md.join(alignment_md)
