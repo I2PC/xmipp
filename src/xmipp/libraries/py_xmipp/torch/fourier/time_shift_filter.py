575ba97d26d7dcf3c44c51f9618c7bd6a5be172d
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2023-02-17 16:45:53 +0100

    Added utility Fourier functions

---
+++ src/xmipp/libraries/py_xmipp/torch/fourier/time_shift_filter.py
@@ -0,0 +1,35 @@
+# ***************************************************************************
+# * Authors:     Oier Lauzirika Zarrabeitia (oierlauzi@bizkaia.eu)
+# *
+# * This program is free software; you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation; either version 2 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# * 02111-1307  USA
+# *
+# *  All comments concerning this program package may be sent to the
+# *  e-mail address 'xmipp@cnb.csic.es'
+# ***************************************************************************/
+
+from typing import Optional
+import torch
+
+def time_shift_filter(shift: torch.Tensor,
+                      freq: torch.Tensor,
+                      out: Optional[torch.Tensor] = None ) -> torch.Tensor:
+    
+    # Fourier time shift theorem:
+    # https://en.wikipedia.org/wiki/Discrete_Fourier_transform#Some_discrete_Fourier_transform_pairs
+    angles = -torch.matmul(shift, freq)
+    gain = torch.tensor(1.0, dtype=angles.dtype, device=angles.device) # TODO try to avoid using this
+    out = torch.polar(gain, angles, out=out)
+    return out
