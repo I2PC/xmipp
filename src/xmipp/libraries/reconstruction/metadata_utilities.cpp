05a0f8484f3ba90fa5c9307cacab9f60f2a52bd6
fede-pe <fepe.ig@gmail.com>
2023-02-24 14:02:07 +0100

    fix union all, allow set operations with empty set

--- src/xmipp/libraries/reconstruction/metadata_utilities.cpp
+++ src/xmipp/libraries/reconstruction/metadata_utilities.cpp
@@ -221,13 +221,26 @@ protected:
 
         if (operation == "union")
         {
+            std::cout << "mdIn.size() " << mdIn.size() << std::endl;
+            std::cout << "md2.size() " << md2.size() << std::endl;
+            std::cout << "label " << label << std::endl;
+            std::cout << "label2 " << label2 << std::endl;
             if(mdIn.isEmpty())
+            {
+                std::cout << "METADATA IS EMPTY!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << std::endl;
                 mdIn = md2;
+            }
             else
                 mdIn.unionDistinct(md2, label);
         }
         else if (operation == "union_all")
-            mdIn.unionAll(md2);
+            if(mdIn.isEmpty())
+            {
+                std::cout << "METADATA IS EMPTY!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << std::endl;
+                mdIn = md2;
+            }
+            else
+                mdIn.unionAll(md2);
         else if (operation == "intersection")
             mdIn.intersection(md2, label);
         else if (operation == "subtraction")
@@ -535,10 +548,10 @@ protected:
 public:
     void run()
     {
-    	if (mdIn.size()==0)
-    		return;
         if (checkParam("--set"))
             doSet();
+        else if (mdIn.size()==0)
+    		return;
         else if (checkParam("--operate"))
             doOperate();
         else if (checkParam("--file"))
