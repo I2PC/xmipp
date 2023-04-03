40b860ca5b49f6072a6a062fc972586d28cb689f
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2023-04-03 08:56:58 +0200

    Removed MetaDataDb from xmippmodule.cpp

--- src/xmipp/bindings/python/xmippmodule.cpp
+++ src/xmipp/bindings/python/xmippmodule.cpp
@@ -25,9 +25,7 @@
 #include "xmippmodule.h"
 #include "core/geometry.h"
 #include "core/matrix2d.h"
-#include "core/metadata_db.h"
-#include "core/metadata_extension.h"
-#include "core/metadata_sql.h"
+#include "core/metadata_vec.h"
 #include "core/transformations.h"
 #include "core/xmipp_image_generic.h"
 #include "core/xmipp_image_extension.h"
@@ -258,7 +256,7 @@ PyObject * xmipp_MetaDataInfo(PyObject *obj, PyObject *args, PyObject *kwargs)
     {
         try
         {
-            MetaData *md = nullptr;
+            MetaDataVec *md = nullptr;
             size_t size; //number of elements in the metadata
             bool destroyMd = true;
 
@@ -266,14 +264,14 @@ PyObject * xmipp_MetaDataInfo(PyObject *obj, PyObject *args, PyObject *kwargs)
             {
                 PyObject* repr = PyObject_Str(pyValue);
                 auto * str = (char*)PyUnicode_AsUTF8(pyValue);
-                md = new MetaDataDb();
+                md = new MetaDataVec();
                 md->setMaxRows(1);
                 md->read(str);
                 size = md->getParsedLines();
             }
             else if (FileName_Check(pyValue))
             {
-                md = new MetaDataDb();
+                md = new MetaDataVec();
                 md->setMaxRows(1);
                 md->read(FileName_Value(pyValue));
                 size = md->getParsedLines();
