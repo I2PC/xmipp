1d8ce22129925e7834f7a2f92a406469ea46d1c5
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2023-04-12 12:00:20 +0200

    Implemented installer for yml conda environments

--- src/xmipp/bindings/python/xmipp_conda_envs.py
+++ src/xmipp/bindings/python/xmipp_conda_envs.py
@@ -1,43 +1,27 @@
+import os
+
+_REQUIREMENT_PATH = os.path.join(os.path.dirname(__file__), 'envs_DLTK')
+
 XMIPP_CONDA_ENVS = {
   "xmipp_DLTK_v0.3": {
-    "pythonVersion": "3.9",
-    "dependencies": ["pandas=1.3.5",
-                     "scikit-image=1.19.3",
-                     "opencv=4.6",
-                     "tensorflow%(gpuTag)s=1.15",
-                     "keras=2.3",
-                     "scikit-learn=0.22"],
-    "channels": ["anaconda"],
-    "pipPackages": {},
-    "defaultInstallOptions": {"gpuTag": ""},
+    "requirements": os.path.join(_REQUIREMENT_PATH, 'xmipp_DLTK_v0.3.yml'),
     "xmippEnviron": True
   },
 
   "xmipp_MicCleaner": {
-    "pythonVersion": "3.6",
-    "dependencies": ["micrograph-cleaner-em=0.35"],
-    "channels": ["rsanchez1369", "anaconda", "conda-forge"],
-    "pipPackages": {},
-    "defaultInstallOptions": {},
+    "requirements": os.path.join(_REQUIREMENT_PATH, 'xmipp_MicCleaner.yml'),
     "xmippEnviron": False
   },
 
   "xmipp_deepEMhancer": {
-    "pythonVersion": "3.6",
-    "dependencies": ["deepemhancer=0.12", "numba=0.45"],
-    "channels": ["rsanchez1369", "anaconda", "conda-forge"],
-    "pipPackages": {},
-    "defaultInstallOptions": {},
+    "requirements": os.path.join(_REQUIREMENT_PATH, 'xmipp_deepEMhancer.yml'),
     "xmippEnviron": False
   },
+  
   "xmipp_deepHand": {
-    "pythonVersion": "3.8",
-    "dependencies": ["pytorch=1.6"],
-    "channels": ["anaconda", "conda-forge"],
-    "pipPackages": {},
-    "defaultInstallOptions": {},
+    "requirements": os.path.join(_REQUIREMENT_PATH, 'xmipp_deepHand.yml'),
     "xmippEnviron": True
-}
+  }
 
 
 }
