a1f0082d82c7aaf6ee8afb883671a8c2414a8e43
Jakub Kiska <kubo.kiska@gmail.com>
2023-04-28 15:25:39 +0200

    Moved memory access to later

--- src/xmipp/libraries/reconstruction_cuda/cuda_forward_art_zernike3d.cu
+++ src/xmipp/libraries/reconstruction_cuda/cuda_forward_art_zernike3d.cu
@@ -428,7 +428,6 @@ __global__ void forwardKernel(const MultidimArrayCuda<PrecisionType> cudaMV,
 	int k = STARTINGZ(cudaMV) + cubeZ;
 	int i = STARTINGY(cudaMV) + cubeY;
 	int j = STARTINGX(cudaMV) + cubeX;
-	PrecisionType weight = A3D_ELEM(cudaMV, k, i, j);
 	PrecisionType gx = 0.0, gy = 0.0, gz = 0.0;
 	if (usesZernike) {
 		auto k2 = k * k;
@@ -458,6 +457,7 @@ __global__ void forwardKernel(const MultidimArrayCuda<PrecisionType> cudaMV,
 
 	auto pos_x = r0 * r_x + r1 * r_y + r2 * r_z;
 	auto pos_y = r3 * r_x + r4 * r_y + r5 * r_z;
+	PrecisionType weight = A3D_ELEM(cudaMV, k, i, j);
 	device::splattingAtPos(pos_x, pos_y, mP, mW, weight);
 }
 
