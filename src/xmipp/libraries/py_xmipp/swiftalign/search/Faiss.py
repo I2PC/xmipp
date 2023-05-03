c328cea651531cbc79f9dfd174d2a5800db6d3d1
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2023-05-03 16:02:17 +0200

    Removed null CUDA stream

--- src/xmipp/libraries/py_xmipp/swiftalign/search/Faiss.py
+++ src/xmipp/libraries/py_xmipp/swiftalign/search/Faiss.py
@@ -97,7 +97,7 @@ class FaissDatabase(Database):
                   use_precomputed = False ):
         if device.type == 'cuda':
             resources = faiss.StandardGpuResources()
-            resources.setDefaultNullStreamAllDevices() # To interop with torch
+            #resources.setDefaultNullStreamAllDevices() # To interop with torch
             co = faiss.GpuClonerOptions()
             co.useFloat16 = use_f16
             #co.useFloat16CoarseQuantizer = use_f16
