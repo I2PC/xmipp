b8f742af558cc11b68ea8eec7c919c93f5452c17
Ratolon <miceta@cnb.csic.es>
2023-06-14 11:10:12 +0200

    Initial script cli params definition

--- src/xmipp/applications/scripts/deep_picking_consensus_tomo/deep_picking_consensus_tomo.py
+++ src/xmipp/applications/scripts/deep_picking_consensus_tomo/deep_picking_consensus_tomo.py
@@ -31,24 +31,65 @@
 import multiprocessing
 import sys
 
-#class ScriptDeepConsensus3D(XmippScript):
-#    _conda_env= CondaEnvManager.CONDA_DEFAULT_ENVIRON
-#    def __init__(self):
-#        XmippScript.__init__(self)
+from xmipp_base import XmippScript
+import xmippLib
 
-class ScriptDeepConsensus3D():
+class ScriptDeepConsensus3D(XmippScript):
+    _conda_env = 'xmipp_DLTK_v1.0'
+
+    def __init__(self):
+        XmippScript.__init__(self)
 
     def defineParams(self):
-        self.addUsageLine()
+        self.addUsageLine('DeepConsensus3D. Launches a CNN to process cryoET (tomo) subvolumes.\n'
+                          'It can be used in these cases:\n'
+                          '1) Train network from scratch\n'
+                          '2) Load a network and keep training\n'
+                          '3) Load a network and score\n'
+                          'Keep in mind that training and scoring are separated, so two separate\n'
+                          'calls to this program are needed for a train+score scenario')
 
         # Application parameters
-        self.addParamsLine('[ -g <gpuId> ]      : (ONLY ONE) GPU Id. (Default: no GPU acceleration)')
-        self.addParamsLine('[ -t <numThreads>   : Number of threads (Default: 4) ]')
-        self.addParamsLine('[ --mode <mode>     : training|scoring ]')
+        self.addParamsLine('==== Application ====')
+        self.addParamsLine('[ -g <gpuId> ]           : GPU Id. Set to -1 to use all CUDA_VISIBLE_DEVICES (Default: no GPU)')
+        self.addParamsLine('[ -t <numThreads> ]      : Number of threads (Default: 4)')
+        self.addParamsLine('[ --mode <mode> }        : training | scoring')
+        self.addParamsLine('[ --netpath <path> ]     : path for network models read/write (needed in any case)')
+
+        # Tomo
+        self.addParamsLine('==== Tomo ====')
+        self.addParamsLine('[ --consboxsize <boxsize> ]     : desired box size (int)')
+        self.addParamsLine('[ --conssamprate <samplingrate>]: desired sampling rate (float)')
+        self.addParamsLine('[]:')
+
+        # Score parameters
+        self.addParamsLine('==== Scoring mode ====')
+        self.addParamsLine('[ -i <path> ]   : path to the metadata of the input subtomos (xmd)')
+        self.addParamsLine('[ -o <path> ]   : path for the program to write the scored coordinates (xmd)')
 
         # Train parameters
+        self.addParamsLine('==== Training mode ====')
+        self.addParamsLine('[ --truevols <path> ]  : path to the positive subtomos (xmd)')
+        self.addParamsLine('[ --falsevols <path>  ]  : path to the negative subtomos (xmd)')
+        self.addParamsLine('[ --trueweights <path> ]  : path to positive subtomos weights (xmd) '
+                            'Default: none - program autoweights')
+        self.addParamsLine('[ --falseweights <path> ]  : path to negative subtomos weights (xmd) '
+                           'Default: none - program autoweights')
+        self.addParamsLine('[ -e <numberOfEpochs> ]  : Number of training epochs (int). Default: 5')
+        self.addParamsLine('[ -l <learningRate> ] : Learning rate (float). Default: 1e-4')
+        self.addParamsLine('[ -r <regStrength> ] : L2 regularization level (float). Default: 1e-5')
+        self.addParamsLine('[ -s <autoStop>]  : Autostop on convergency detection (boolean). Default: True')
+        self.addParamsLine('[ --ensemble <numberOfModels> ] : If set, an ensemble of models will be used in a voting instead one.')
 
         # Use examples
+        self.addExampleLine('Training the network from scratch\n'
+                            'xmipp_deep_picking_consensus_tomo')
+        
+        self.addExampleLine('Keep training the network from previous run\n'
+                            'xmipp_deep_picking_consensus_tomo')
+        
+        self.addExampleLine('Training the network from scratch\n'
+                            'xmipp_deep_picking_consensus_tomo -')
 
     def run(self):
         numThreads = None
