0eb1e2b5ab600d7a5764d77fcc6bbe434e73026e
Ratolon <miceta@cnb.csic.es>
2023-06-16 00:39:36 +0200

    Several fixes

--- src/xmipp/applications/scripts/deep_picking_consensus_tomo/deep_picking_consensus_tomo.py
+++ src/xmipp/applications/scripts/deep_picking_consensus_tomo/deep_picking_consensus_tomo.py
@@ -36,12 +36,16 @@ import numpy as np
 from xmipp_base import XmippScript
 import xmippLib
 
-MODEL_TRAIN_NEW = 0
-MODEL_TRAIN_PRETRAIN = 1
-MODEL_TRAIN_PREVRUN = 2
-MODEL_TRAIN_TYPELIST= ["From scratch", "Existing model", "Previous run"]
+from xmippPyModules.deepPickingConsensusTomo.deepPickingConsensusTomo_networks import DeepPickingConsensusTomoNetworkManager as NM
+from xmippPyModules.deepPickingConsensusTomo.deepPickingConsensusTomo_dataman import DataMan
+
+MODEL_TRAIN_NEW=0
+MODEL_TRAIN_PRETRAIN=1
+MODEL_TRAIN_PREVRUN=2
+MODEL_TRAIN_TYPELIST=["From scratch", "Existing model", "Previous run"]
 
 class ScriptDeepConsensus3D(XmippScript):
+    
     _conda_env = 'xmipp_DLTK_v1.0'
 
     # Type the parameters
@@ -81,31 +85,31 @@ class ScriptDeepConsensus3D(XmippScript):
 
         # Application parameters
         self.addParamsLine('==== Application ====')
-        self.addParamsLine('[ -t <numThreads> ]      : Number of threads (Default: 4)')
-        self.addParamsLine('[ -g <gpuId> ]           : space separated GPU Ids. Set to -1 to use all CUDA_VISIBLE_DEVICES (Default: no GPU)') 
-        self.addParamsLine('[ --mode <mode> }        : training | scoring')
-        self.addParamsLine('[ --netpath <path> ]     : path for network models read/write (needed in any case)')
+        self.addParamsLine('[ -t <numThreads=4> ] : Number of threads')
+        self.addParamsLine(' -g <gpuId> : comma separated GPU Ids. Set to -1 to use all CUDA_VISIBLE_DEVICES') 
+        self.addParamsLine(' --mode <execMode> : training or scoring')
+        self.addParamsLine(' --netpath <netpath> : path for network models read/write (needed in any case)')
 
         # Tomo
         self.addParamsLine('==== Tomo ====')
-        self.addParamsLine('[ --consboxsize <boxsize> ]     : desired box size (int)')
-        self.addParamsLine('[ --conssamprate <samplingrate>]: desired sampling rate (float)')
-        self.addParamsLine('[]:')
+        self.addParamsLine(' --consboxsize <boxsize> : desired box size (int)')
+        self.addParamsLine(' --conssamprate <samplingrate> : desired sampling rate (float)')
 
         # Score parameters
         self.addParamsLine('==== Scoring mode ====')
-        self.addParamsLine('[ -i <path> ]   : path to the metadata of the input subtomos (xmd)')
+        self.addParamsLine('[ -i <path> ]   : path to the metadata of the input doubtful subtomos (mrc)')
         self.addParamsLine('[ -o <path> ]   : path for the program to write the scored coordinates (xmd)')
 
         # Train parameters
         self.addParamsLine('==== Training mode ====')
-        self.addParamsLine('[ --truevolfile <path> ]  : path to the positive subtomos (xmd)')
-        self.addParamsLine('[ --falsevolfile <path>  ]  : path to the negative subtomos (xmd)')
-        self.addParamsLine('[ -e <numberOfEpochs> ]  : Number of training epochs (int). Default: 5')
-        self.addParamsLine('[ -l <learningRate> ] : Learning rate (float). Default: 1e-4')
-        self.addParamsLine('[ -r <regStrength> ] : L2 regularization level (float). Default: 1e-5')
-        self.addParamsLine('[ -s <autoStop>]  : Autostop on convergency detection (boolean). Default: True')
-        self.addParamsLine('[ --ensemble <numberOfModels> ] : If set, an ensemble of models will be used in a voting instead one.')
+        self.addParamsLine(' --ttype <traintype=0> : train mode')
+        self.addParamsLine(' --truevolpath <truevolpath> : path to the positive subtomos (mrc)')
+        self.addParamsLine(' --falsevolpath <falsevolpath> : path to the negative subtomos (mrc)')
+        self.addParamsLine('[ -e <numberOfEpochs=5> ]  : Number of training epochs (int).')
+        self.addParamsLine('[ -l <learningRate=0.0001> ] : Learning rate (float).')
+        self.addParamsLine('[ -r <regStrength=0.00001> ] : L2 regularization level (float).')
+        self.addParamsLine('[ -s <NoAutoStop>] : Do not autostop on convergency detection.')
+        self.addParamsLine('[ --ensemble <numberOfModels=1> ] : If set, an ensemble of models will be used in a voting instead one.')
 
         # Use examples
         self.addExampleLine('Training the network from scratch\n'
@@ -130,30 +134,24 @@ class ScriptDeepConsensus3D(XmippScript):
             sysThreads = multiprocessing.cpu_count()
             self.numThreads = min(sysThreads, 4)
 
-        # Default for GPU: no GPU
-        if self.checkParam('-g'):
-            # GPU acceleration
-            gpustr : str = self.getParam('-g')
-            self.gpus : list = [ int(item) for item in gpustr.split()]
-            # -1 means all CUDA_VISIBLE_DEVICES must be used
-            if -1 in self.gpus:
-                if 'CUDA_VISIBLE_DEVICES' in os.environ:
-                    gpustr = os.environ.get('CUDA_VISIBLE_DEVICES')
-                    self.gpus : list = [int(item) for item in gpustr.split(",")]
-        else:
-            # No GPU acceleration
-            self.gpus = []
+        # GPU acceleration
+        gpustr : str = self.getParam('-g')
+        self.gpus : list = [ int(item) for item in gpustr.split(",")]
+        # -1 means all CUDA_VISIBLE_DEVICES must be used
+        if -1 in self.gpus:
+            if 'CUDA_VISIBLE_DEVICES' in os.environ:
+                gpustr = os.environ.get('CUDA_VISIBLE_DEVICES')
+                self.gpus : list = [int(item) for item in gpustr.split(",")]
 
         # Netpath
-        if self.checkParam('--netpath'):
-            self.netpath = self.getParam('--netpath')
-            if not os.path.isdir(self.netpath):
-                print("Network path is not a valid path")
-                sys.exit(-1)
+        self.netpath = self.getParam('--netpath')
+        if not os.path.isdir(self.netpath):
+            print("Network path is not a valid path")
+            sys.exit(-1)
         
         # Consensuated box size
         if self.checkParam('--consboxsize'):
-            pass
+            self.consboxsize : int = self.getIntParam('--consboxsize')
 
         # Consensuated sampling rate
         if self.checkParam('--conssamprate'):
@@ -189,32 +187,30 @@ class ScriptDeepConsensus3D(XmippScript):
                 # General training checks
                 # When training, you need to read the pos and neg paths as well as create
                 # the needed weights (for the future) - for any of the cases
-                truestring : str = self.getParam('--truevolfiles')
-                falsestring : str = self.getParam('--falsevolfiles')
-                self.truevolpaths = [ fn for fn in truestring.split(":") if os.path.isfile(fn) ]
-                self.falsevolpaths = [ fn for fn in falsestring.split(":") if os.path.isfile(fn) ]
+                self.posPath : str = self.getParam('--truevolpath')
+                self.negPath : str = self.getParam('--falsevolpath')
                 
-                # Assign weights to items if variable is set
-                if self.checkParam('--trueweights'):
-                    tw : str = self.getParam('--trueweights')
-                    self.trueweights = np.array(tw.split(":"), dtype=int).tolist()
-                else:
-                    self.trueweights = [-1] * len(self.truevolpaths)
-                if self.checkParam('--falseweights'):
-                    tw : str = self.getParam('--falseweights')
-                    self.falseweights = np.array(tw.split(":"), dtype=int).tolist()
-                else:
-                    self.falseweights = [-1] * len(self.falsevolpaths)
+                # # Assign weights to items if variable is set
+                # if self.checkParam('--trueweights'):
+                #     tw : str = self.getParam('--trueweights')
+                #     self.trueweights = np.array(tw.split(":"), dtype=int).tolist()
+                # else:
+                #     self.trueweights = [-1] * len(self.truevolpaths)
+                # if self.checkParam('--falseweights'):
+                #     tw : str = self.getParam('--falseweights')
+                #     self.falseweights = np.array(tw.split(":"), dtype=int).tolist()
+                # else:
+                #     self.falseweights = [-1] * len(self.falsevolpaths)
                 
                 # Check size
-                msg_weights = "Error, the number of weights provided does not match the amount of files provided."+\
-                            "Check --trueweights and --falseweights"
-                assert len(self.falseweights) == len(self.truevolpaths), msg_weights
-                assert len(self.trueweights) == len(self.falsevolpaths), msg_weights
+                # msg_weights = "Error, the number of weights provided does not match the amount of files provided."+\
+                #            "Check --trueweights and --falseweights"
+                # assert len(self.falseweights) == len(self.truevolpaths), msg_weights
+                # assert len(self.trueweights) == len(self.falsevolpaths), msg_weights
 
                 # Create the dictionaries
-                self.posTrainDict = {path: weight for path, weight in zip(self.truevolpaths, self.trueweights)}
-                self.negTrainDict = {path: weight for path, weight in zip(self.falsevolpaths, self.falseweights)}
+                # self.posTrainDict = {path: weight for path, weight in zip(self.truevolpaths, self.trueweights)}
+                # self.negTrainDict = {path: weight for path, weight in zip(self.falsevolpaths, self.falseweights)}
 
                 # Epochs for training
                 if self.checkParam('-e'):
@@ -242,7 +238,7 @@ class ScriptDeepConsensus3D(XmippScript):
 
                 # Ensemble amount
                 if self.checkParam('--ensemble'):
-                    self.ensemble = int(self.getIntParam('--ensemble'))
+                    self.ensemble = self.getIntParam('--ensemble')
                 else:
                     self.ensemble = 1
 
@@ -265,24 +261,25 @@ class ScriptDeepConsensus3D(XmippScript):
                     print("No output name given. Defaulting to scored_output.xmd")
                     self.outputfname = "scored_output.xmd"
                 
-
     def run(self):
 
-        print("deep_picking_consensus_tomo.py run() is launched\nParsing input...")
+        print("deep_picking_consensus_tomo.py is launched\nParsing input...")
         self.parseParams()
         print("Execution will be done using %d threads." % self.numThreads)
         gpustring = str(self.gpus)
         print("Execution will use GPUS with ID: " + gpustring)
 
         if self.mode == "train":
+            # if self.traintype == MODEL_TRAIN_NEW:
+            #     pass
+            # elif self.traintype == MODEL_TRAIN_PRETRAIN:
+            #     pass
+            # elif self.traintype == MODEL_TRAIN_PREVRUN:
+            #     pass
 
-            # TODO: generate args for train or score
-            if self.traintype == MODEL_TRAIN_NEW:
-                pass
-            elif self.traintype == MODEL_TRAIN_PRETRAIN:
-                pass
-            elif self.traintype == MODEL_TRAIN_PREVRUN:
-                pass
+            # splits
+            dataMan = DataMan(self.posPath, self.negPath, self.consboxsize)
+            self.doTrain(dataMan)
 
         elif self.mode == "score":
             pass
@@ -293,21 +290,13 @@ class ScriptDeepConsensus3D(XmippScript):
 
         sys.exit(0)
 
-    @staticmethod
-    def trainingWorker():
-        # TODO: definir entradas
+    def doTrain(self, dataMan):
+        
+        netMan = NM(self.numThreads, self.gpus, self.netpath)
+        netMan.createNetwork(self.consboxsize, self.consboxsize, self.consboxsize, 1)
+
+    def doScore():
         pass
-
-    @staticmethod
-    def scoringWorker(netDataPath:str, posTestDict: dict, negTestDict:dict, predictDict: dict,
-                      outVolsPath:str, gpus:list, nthreads:int):
-        
-        # Set the thread number
-        systemThreads = multiprocessing.cpu_count()
-        threads = min(nthreads, systemThreads)
-
-        
-
 
 if __name__ == '__main__':
     exitCode = ScriptDeepConsensus3D().tryRun()
