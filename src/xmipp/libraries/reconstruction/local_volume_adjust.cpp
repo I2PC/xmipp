f33f7324a0024fc066e68759a19fa22325c85707
EstrellaFG <estrellafergi@gmail.com>
2023-07-06 19:29:44 +0200

    more on progress

--- src/xmipp/libraries/reconstruction/local_volume_adjust.cpp
+++ src/xmipp/libraries/reconstruction/local_volume_adjust.cpp
@@ -71,72 +71,78 @@ void ProgLocalVolumeAdjust::run() {
     Image<double> Vref;
 	Vref.read(fnVol1);
 	MultidimArray<double> &mVref=Vref();
+	mVref.setXmippOrigin();
 	Image<double> V;
 	V.read(fnVol2);
 	MultidimArray<double> &mV=V();
+	mV.setXmippOrigin();
 	Image<double> M;
 	M.read(fnMask);
 	MultidimArray<double> &mM=M();
-	std::cout << "----0---" << std::endl;
-	//std::cout << "----neighborhood = " << neighborhood << std::endl;
+	mM.setXmippOrigin();
+	int iters;
+	int cubic_neighborhood;
+	cubic_neighborhood = neighborhood*neighborhood*neighborhood;
+	iters = floor(ZYXSIZE(mV)/cubic_neighborhood);
+	std::cout << "----iters: " << iters << std::endl;
+	int xsize = XSIZE(mV);
+	int ysize = YSIZE(mV);
+	int zsize = ZSIZE(mV);
 	k=0;
-	j=0;
 	i=0;
-	std::cout << "----1---" << std::endl;
-	for (size_t s; s <= s<ZSIZE(mV); s+(neighborhood*neighborhood*neighborhood)) //TODO: check
+	j=0;
+	for (size_t s=0; s <= iters; s++) //TODO: check
 	{
-		std::cout << "----2---" << std::endl;
 		// Compute adjustment (c) TODO: per regions (neighborhood)
 		sumV_Vref = 0;
 		sumV_Vref = 0;
-		for (k; k <= neighborhood; ++k)
+		
+		for (k=0; k <= neighborhood; ++k)
 		{
-			std::cout << "----3---" << std::endl;
-			for (i; i <= neighborhood; ++i)
+			for (i=0; i <= neighborhood; ++i)
 			{
-				std::cout << "----4---" << std::endl;
-				for (j; j <= neighborhood; ++j)
+				for (j=0; j <= neighborhood; ++j)
 				{
-					std::cout << "----5---" << std::endl;
+					std::cout << "-------" << std::endl;
 					std::cout << "k = "<< k << std::endl;
 					std::cout << "i = "<< i << std::endl;
 					std::cout << "j = "<< j << std::endl;
 					if (DIRECT_A3D_ELEM(mM,k,i,j)==1) // Condition to check if we are inside mask
 					{
-						std::cout << "----6---" << std::endl;
 						sumV_Vref += DIRECT_A3D_ELEM(mV,k,i,j)*DIRECT_A3D_ELEM(mVref,k,i,j);
-						std::cout << "----7---" << std::endl;
 						sumVref2 += DIRECT_A3D_ELEM(mVref,k,i,j)*DIRECT_A3D_ELEM(mVref,k,i,j);
-						std::cout << "----8---" << std::endl;
 					}
 					else //TODO: check
 					{
-						std::cout << "----66---" << std::endl;
 						sumV_Vref += 0;
-						std::cout << "----77---" << std::endl;
 						sumVref2 += 0;
-						std::cout << "----88---" << std::endl;
 					}
 				}     
 			}
 		}
-		std::cout << "----9---" << std::endl;
+		std::cout << "-------" << std::endl;
+		std::cout << "sumV_Vref = "<< sumV_Vref << std::endl;
+		std::cout << "sumVref2 = "<< sumVref2 << std::endl;
 		c = sumV_Vref/sumVref2;
-		std::cout << "----10---" << std::endl;
-		// Apply adjustment
-		FOR_ALL_DIRECT_ELEMENTS_IN_MULTIDIMARRAY(mVref) 
+		std::cout << "c = "<< c << std::endl;
+		// Apply adjustment TODO: per regions
+		for (size_t ki=0; ki <= neighborhood; ++ki)
 		{
-			DIRECT_MULTIDIM_ELEM(mV, n) *= c;
-		} 
+			for (size_t ii=0; ii <= neighborhood; ++ii)
+			{
+				for (size_t ji=0; ji <= neighborhood; ++ji)
+				{
+					DIRECT_A3D_ELEM(mV,ki,ii,ji) *= c;
+				}
+			}
+		}
 	}
-	std::cout << "----11---" << std::endl;
+
 	// The output of this program is a modified version of V (V')
 	if (performSubtraction) // Or the output is the subtraction V = Vref - V
 	{
-		std::cout << "----12---" << std::endl;
 		FOR_ALL_DIRECT_ELEMENTS_IN_MULTIDIMARRAY(mV)
 			DIRECT_MULTIDIM_ELEM(mV,n) = DIRECT_MULTIDIM_ELEM(mV,n)-DIRECT_MULTIDIM_ELEM(mVref,n);
 	}
-	std::cout << "----13---" << std::endl;
 	V.write(fnOutVol);
 }
