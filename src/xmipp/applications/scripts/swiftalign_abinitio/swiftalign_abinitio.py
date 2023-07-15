aadd53f7d724b25b1137c6addc424b6ae6ab5553
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2023-07-15 15:51:56 +0000

    Added input

--- src/xmipp/applications/scripts/swiftalign_abinitio/swiftalign_abinitio.py
+++ src/xmipp/applications/scripts/swiftalign_abinitio/swiftalign_abinitio.py
@@ -23,17 +23,41 @@
 # ***************************************************************************/
 
 import torch
+import argparse
 
 import xmippPyModules.swiftalign.metadata as md
 import xmippPyModules.swiftalign.image as image
 import xmippPyModules.swiftalign.commonlines as commonlines
 
 def run(images_md_path: str,
-        n_angles: int):
+        n_angles: int,
+        batch: int,
+        n_iter: int):
     images_md = md.sort_by_image_filename(md.read(images_md_path))
     images_paths = list(map(image.parse_path, images_md[md.IMAGE]))
     images_dataset = image.torch_utils.Dataset(images_paths)
     images = torch.utils.data.default_collate(images_dataset)
     
     sinogram = commonlines.compute_sinogram_2d(images, n_angles, 'bilinear')
-    matrices, error = commonlines.optimize_common_lines_monte_carlo(sinogram, 2**16, 8192)
+    matrices, error = commonlines.optimize_common_lines_monte_carlo(sinogram, n_iter, batch)
+    
+if __name__ == '__main__':
+    # Define the input
+    parser = argparse.ArgumentParser(
+                        prog = 'Ab initio volume using common lines')
+    parser.add_argument('-i', required=True)
+    parser.add_argument('--sinogram_steps', type=int, default=72)
+    parser.add_argument('--batch', type=int, default=1024)
+    parser.add_argument('--iter', type=int, default=2**16)
+
+
+    # Parse
+    args = parser.parse_args()
+
+    # Run the program
+    run(
+        images_md_path=args.i,
+        n_angles=args.sinogram_steps,
+        batch=args.batch,
+        n_iter=args.iter
+    )
