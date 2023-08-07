b74b1b1059888e6f76b6a5bd396988fa61378d4d
Ratolon <miceta@cnb.csic.es>
2023-08-07 20:49:49 +0200

    Input parameter tuning

--- src/xmipp/applications/scripts/deep_picking_consensus_tomo/deep_picking_consensus_tomo.py
+++ src/xmipp/applications/scripts/deep_picking_consensus_tomo/deep_picking_consensus_tomo.py
@@ -44,6 +44,10 @@ MODEL_TRAIN_PRETRAIN    = 1
 MODEL_TRAIN_PREVRUN     = 2
 MODEL_TRAIN_TYPELIST    = ["From scratch", "Existing model", "Previous run"]
 
+NN_TRAINWORDS           = ["train", "training", "t"]
+NN_SCOREWORDS           = ["score", "scoring", "s", "predict"]
+
+DEFAULT_MP              = 8
 
 class ScriptDeepConsensus3D(XmippScript):
     
@@ -98,8 +102,8 @@ class ScriptDeepConsensus3D(XmippScript):
 
         # Score parameters
         self.addParamsLine('==== Scoring mode ====')
-        self.addParamsLine('[ -i <path> ]   : path to the metadata of the input doubtful subtomos (mrc)')
-        self.addParamsLine('[ -o <path> ]   : path for the program to write the scored coordinates (xmd)')
+        self.addParamsLine('[ --inputvolpath <path> ]   : path to the metadata files of the input doubtful subtomos (mrc)')
+        self.addParamsLine('[ --outputfile <path> ]   : path for the program to write the scored coordinates (xmd)')
 
         # Train parameters
         self.addParamsLine('==== Training mode ====')
@@ -109,7 +113,7 @@ class ScriptDeepConsensus3D(XmippScript):
         self.addParamsLine('[ -e <numberOfEpochs=5> ]  : Number of training epochs (int).')
         self.addParamsLine('[ -l <learningRate=0.0001> ] : Learning rate (float).')
         self.addParamsLine('[ -r <regStrength=0.00001> ] : L2 regularization level (float).')
-        self.addParamsLine('[ -s <NoAutoStop>] : Do not autostop on convergency detection.')
+        self.addParamsLine('[ -s <AutoStop>] : Autostop on convergency detection.')
         self.addParamsLine('[ --ensemble <numberOfModels=1> ] : If set, an ensemble of models will be used in a voting instead one.')
 
         # Use examples
@@ -132,11 +136,8 @@ class ScriptDeepConsensus3D(XmippScript):
         if self.checkParam('-t'):
             self.numThreads = self.getIntParam('-t')
         else:
-            #sysThreads = multiprocessing.cpu_count()
-            #self.numThreads = min(sysThreads, 4)
-            self.numThreads = 4
-
-        # GPU acceleration
+            self.numThreads = DEFAULT_MP
+        # GPU acceleration - assuming GPU is always used
         gpustr : str = self.getParam('-g')
         self.gpus : list = [ int(item) for item in gpustr.split(",")]
         # -1 means all CUDA_VISIBLE_DEVICES must be used
@@ -144,124 +145,89 @@ class ScriptDeepConsensus3D(XmippScript):
             if 'CUDA_VISIBLE_DEVICES' in os.environ:
                 gpustr = os.environ.get('CUDA_VISIBLE_DEVICES')
                 self.gpus : list = [int(item) for item in gpustr.split(",")]
-
+        # Execution mode
+        self.execMode = str(self.getParam('--mode'))
         # Netpath
         self.netpath = self.getParam('--netpath')
         if not os.path.isdir(self.netpath):
             print("Network path is not a valid path")
             sys.exit(-1)
-        
-        # Consensuated box size
-        if self.checkParam('--consboxsize'):
-            self.consboxsize : int = self.getIntParam('--consboxsize')
-
-        # Consensuated sampling rate
-        if self.checkParam('--conssamprate'):
-            pass
+        # Consensuated boxsize and sampling ratesize
+        self.consboxsize : int = self.getIntParam('--consboxsize')
+        self.consSampRate : float = self.getDoubleParam('--conssamprate')
        
-        # Mode
-        if self.checkParam('--mode'):
-            self.mode = str(self.getParam('--mode'))
-            self.mode = self.mode.strip()
+        # The desired running mode is training
+        if self.execMode.strip() in NN_TRAINWORDS:
+            self.execMode = "train"
+            print("Execution mode is: TRAINING")
 
-            # Define several words for the same 
-            trainwords = ["train", "training", "t"]
-            scorewords = ["score", "scoring", "s", "predict"]
+            # Training type
+            self.trainType = str(self.getParam('--ttype'))
+            # Read paths
+            self.posPath : str = self.getParam('--truevolpath')
+            self.negPath : str = self.getParam('--falsevolpath')
+            # Learning rate
+            if self.checkParam('-l'):
+                self.learningRate = float(self.getDoubleParam('-l'))
+            else:
+                self.learningRate = 1.0e-4
+            # Epochs for training
+            if self.checkParam('-e'):
+                self.nEpochs = int(self.getIntParam('-e'))
+            else:
+                self.nEpochs = 5
+            # Regularization strenght
+            if self.checkParam('-r'):
+                self.regStrength= float(self.getDoubleParam('-r'))
+            else:
+                self.regStrength = 1.0e-5
+            # Auto stop feature
+            self.autoStop = self.checkParam('-s')
+            # Ensemble amount
+            if self.checkParam('--ensemble'):
+                self.ensemble = self.getIntParam('--ensemble')
+            else:
+                self.ensemble = 1
+            # Check if it's in the correct range and set to default if needed
+            if self.traintype not in [0, 1, 2]:
+                print("Training mode %d not recognized. Running a new model instead." % self.traintype)
+                self.traintype = MODEL_TRAIN_NEW
+            else:
+                print("Training in mode: " +  MODEL_TRAIN_TYPELIST[self.traintype])
+                          
+            # # Assign weights to items if variable is set
+            # if self.checkParam('--trueweights'):
+            #     tw : str = self.getParam('--trueweights')
+            #     self.trueweights = np.array(tw.split(":"), dtype=int).tolist()
+            # else:
+            #     self.trueweights = [-1] * len(self.truevolpaths)
+            # if self.checkParam('--falseweights'):
+            #     tw : str = self.getParam('--falseweights')
+            #     self.falseweights = np.array(tw.split(":"), dtype=int).tolist()
+            # else:
+            #     self.falseweights = [-1] * len(self.falsevolpaths)
+            
+            # Check size
+            # msg_weights = "Error, the number of weights provided does not match the amount of files provided."+\
+            #            "Check --trueweights and --falseweights"
+            # assert len(self.falseweights) == len(self.truevolpaths), msg_weights
+            # assert len(self.trueweights) == len(self.falsevolpaths), msg_weights
 
-            # The desired running mode is training
-            if self.mode in trainwords:
-                print("Execution mode is: TRAINING")
-                self.mode = "train"
+            # Create the dictionaries
+            # self.posTrainDict = {path: weight for path, weight in zip(self.truevolpaths, self.trueweights)}
+            # self.negTrainDict = {path: weight for path, weight in zip(self.falsevolpaths, self.falseweights)}
 
-                # Determine the kind of training
-                if self.checkParam('--ttype'):
-                    self.traintype = int(self.getIntParam('--ttype'))
-                    # Check if it's in the correct range and set to default if needed
-                    if self.traintype not in [0, 1, 2]:
-                        print("Training mode %d not recognized. Running a new model instead." % self.traintype)
-                        self.traintype = MODEL_TRAIN_NEW
-                    else:
-                        print("Training in mode: " +  MODEL_TRAIN_TYPELIST[self.traintype])
-                else: # If not specified, start from scratch
-                    print("No training mode specified. Running a new model instead")
-                    self.traintype = MODEL_TRAIN_NEW
+        # The desired mode is scoring
+        elif self.execMode.strip() in NN_SCOREWORDS:
+            print("Execution mode is: SCORING")
+            self.execMode = "score"
 
-                # General training checks
-                # When training, you need to read the pos and neg paths as well as create
-                # the needed weights (for the future) - for any of the cases
-                self.posPath : str = self.getParam('--truevolpath')
-                self.negPath : str = self.getParam('--falsevolpath')
-                
-                # # Assign weights to items if variable is set
-                # if self.checkParam('--trueweights'):
-                #     tw : str = self.getParam('--trueweights')
-                #     self.trueweights = np.array(tw.split(":"), dtype=int).tolist()
-                # else:
-                #     self.trueweights = [-1] * len(self.truevolpaths)
-                # if self.checkParam('--falseweights'):
-                #     tw : str = self.getParam('--falseweights')
-                #     self.falseweights = np.array(tw.split(":"), dtype=int).tolist()
-                # else:
-                #     self.falseweights = [-1] * len(self.falsevolpaths)
-                
-                # Check size
-                # msg_weights = "Error, the number of weights provided does not match the amount of files provided."+\
-                #            "Check --trueweights and --falseweights"
-                # assert len(self.falseweights) == len(self.truevolpaths), msg_weights
-                # assert len(self.trueweights) == len(self.falsevolpaths), msg_weights
-
-                # Create the dictionaries
-                # self.posTrainDict = {path: weight for path, weight in zip(self.truevolpaths, self.trueweights)}
-                # self.negTrainDict = {path: weight for path, weight in zip(self.falsevolpaths, self.falseweights)}
-
-                # Epochs for training
-                if self.checkParam('-e'):
-                    self.nepochs = int(self.getIntParam('-e'))
-                else:
-                    self.nepochs = 5
-
-                # Learning rate
-                if self.checkParam('-l'):
-                    self.learningrate = float(self.getDoubleParam('-l'))
-                else:
-                    self.learningrate = 1.0e-4
-
-                # Regularization strenght
-                if self.checkParam('-r'):
-                    self.regstrength= float(self.getDoubleParam('-r'))
-                else:
-                    self.regstrength = 1.0e-5
-
-                # Auto stop feature
-                if self.checkParam('-s'):
-                    self.autostop = bool(self.getParam('-s'))
-                else:
-                    self.autostop = True
-
-                # Ensemble amount
-                if self.checkParam('--ensemble'):
-                    self.ensemble = self.getIntParam('--ensemble')
-                else:
-                    self.ensemble = 1
-
-
-            # The desired mode is scoring
-            elif self.mode in scorewords:
-                print("Execution mode is: SCORING")
-                self.mode = "score"
-
-                if self.checkParam('-i'):
-                    pass
-                else:
-                    print("No input volume files found")
-                    sys.exit(-1)
-                
-                # Output naming
-                if self.checkParam('-o'):
-                    self.outputfname = self.getParam('-o')
-                else:
-                    print("No output name given. Defaulting to scored_output.xmd")
-                    self.outputfname = "scored_output.xmd"
+            # Input/Output
+            self.inputVolPath = str(self.getParam('--inputvolpath'))
+            if not os.path.exists(self.inputVolPath):
+                print("Path to input subtomograms does not exist. Exiting.")
+                sys.exit(-1)
+            self.outputFile = str(self.getParam('--outputfile'))
                 
     def run(self):
 
