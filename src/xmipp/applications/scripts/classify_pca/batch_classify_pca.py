8c774e8756a62a103fc952d81c51d85ab25b3cb5
erneyramirez <erney.ramirez@gmail.com>
2023-09-21 16:13:17 +0200

    bigger particles

--- src/xmipp/applications/scripts/classify_pca/batch_classify_pca.py
+++ src/xmipp/applications/scripts/classify_pca/batch_classify_pca.py
@@ -93,8 +93,8 @@ if __name__=="__main__":
     if mask and sigma is None:
         sigma = dim/3
     
-    expBatchSize = 5000 
-    numFirstBatch = 6
+    expBatchSize = 3072 
+    numFirstBatch = 10
     initSubset = min(30000, nExp)
     refClas = torch.zeros(nExp)
     translation_vector = torch.zeros(nExp, 2)
