b887f7864d92681047136adce516b773fe26aeb8
Carlos Oscar Sorzano <coss@cnb.csic.es>
2023-11-23 11:35:37 +0100

    Some Rotational tools added

--- src/xmipp/libraries/py_xmipp/xmipp_utils/__init__.py
+++ src/xmipp/libraries/py_xmipp/xmipp_utils/__init__.py
@@ -28,6 +28,7 @@
 import math
 import numpy as np
 from scipy.ndimage import affine_transform
+from scipy.spatial.transform import Rotation
 
 def applyTransformNP(img, rot, x, y):
     "Transform a numpy array with rot, x and y"
@@ -35,3 +36,156 @@ def applyTransformNP(img, rot, x, y):
     c, s = math.cos(theta), math.sin(theta)
     M = np.array([[c, -s, x], [s, c, y], [0, 0, 1]])
     return affine_transform(img, M, order=1, mode='reflect', output_shape=img.shape)
+
+class RotationHandler:
+    """ This class can convert from quaternions to any Euler convention and viceversa.
+        It can also compute the average of several quaternions and calculate the distance of the
+        different quaternions to the average."""
+    _AXES2TUPLE = {
+        'sxyz': (0, 0, 0, 0), 'sxyx': (0, 0, 1, 0), 'sxzy': (0, 1, 0, 0),
+        'sxzx': (0, 1, 1, 0), 'syzx': (1, 0, 0, 0), 'syzy': (1, 0, 1, 0),
+        'syxz': (1, 1, 0, 0), 'syxy': (1, 1, 1, 0), 'szxy': (2, 0, 0, 0),
+        'szxz': (2, 0, 1, 0), 'szyx': (2, 1, 0, 0), 'szyz': (2, 1, 1, 0),
+        'rzyx': (0, 0, 0, 1), 'rxyx': (0, 0, 1, 1), 'ryzx': (0, 1, 0, 1),
+        'rxzx': (0, 1, 1, 1), 'rxzy': (1, 0, 0, 1), 'ryzy': (1, 0, 1, 1),
+        'rzxy': (1, 1, 0, 1), 'ryxy': (1, 1, 1, 1), 'ryxz': (2, 0, 0, 1),
+        'rzxz': (2, 0, 1, 1), 'rxyz': (2, 1, 0, 1), 'rzyz': (2, 1, 1, 1)}
+
+    _TUPLE2AXES = dict((v, k) for k, v in _AXES2TUPLE.items())
+    # axis sequences for Euler angles
+    _NEXT_AXIS = [1, 2, 0, 1]
+
+    _EPS = np.finfo(float).eps * 4.0
+
+    @staticmethod
+    def quaternion_from_matrix(matrix, isprecise=False):
+        """Return quaternion from rotation matrix.
+
+        If isprecise is True, the input matrix is assumed to be a precise rotation
+        matrix and a faster algorithm is used."""
+        M = np.array(matrix, dtype=np.float64, copy=False)[:4, :4]
+        if isprecise:
+            q = np.empty((4,))
+            t = np.trace(M)
+            if t > M[3, 3]:
+                q[0] = t
+                q[3] = M[1, 0] - M[0, 1]
+                q[2] = M[0, 2] - M[2, 0]
+                q[1] = M[2, 1] - M[1, 2]
+            else:
+                i, j, k = 1, 2, 3
+                if M[1, 1] > M[0, 0]:
+                    i, j, k = 2, 3, 1
+                if M[2, 2] > M[i, i]:
+                    i, j, k = 3, 1, 2
+                t = M[i, i] - (M[j, j] + M[k, k]) + M[3, 3]
+                q[i] = t
+                q[j] = M[i, j] + M[j, i]
+                q[k] = M[k, i] + M[i, k]
+                q[3] = M[k, j] - M[j, k]
+            q *= 0.5 / math.sqrt(t * M[3, 3])
+        else:
+            m00 = M[0, 0]
+            m01 = M[0, 1]
+            m02 = M[0, 2]
+            m10 = M[1, 0]
+            m11 = M[1, 1]
+            m12 = M[1, 2]
+            m20 = M[2, 0]
+            m21 = M[2, 1]
+            m22 = M[2, 2]
+            # symmetric matrix K
+            K = np.array([[m00 - m11 - m22, 0.0, 0.0, 0.0],
+                          [m01 + m10, m11 - m00 - m22, 0.0, 0.0],
+                          [m02 + m20, m12 + m21, m22 - m00 - m11, 0.0],
+                          [m21 - m12, m02 - m20, m10 - m01, m00 + m11 + m22]])
+            K /= 3.0
+            # quaternion is eigenvector of K that corresponds to largest eigenvalue
+            w, V = np.linalg.eigh(K)
+            q = V[[3, 0, 1, 2], np.argmax(w)]
+        if q[0] < 0.0:
+            np.negative(q, q)
+        return q
+
+    @staticmethod
+    def quaternion_matrix(quaternion):
+        """Return homogeneous rotation matrix from quaternion."""
+        q = np.array(quaternion, dtype=np.float64, copy=True)
+        n = np.dot(q, q)
+        if n < RotationHandler._EPS:
+            return np.identity(4)
+        q *= math.sqrt(2.0 / n)
+        q = np.outer(q, q)
+        return np.array([
+            [1.0 - q[2, 2] - q[3, 3], q[1, 2] - q[3, 0], q[1, 3] + q[2, 0], 0.0],
+            [q[1, 2] + q[3, 0], 1.0 - q[1, 1] - q[3, 3], q[2, 3] - q[1, 0], 0.0],
+            [q[1, 3] - q[2, 0], q[2, 3] + q[1, 0], 1.0 - q[1, 1] - q[2, 2], 0.0],
+            [0.0, 0.0, 0.0, 1.0]])
+
+    @staticmethod
+    def euler_from_matrix(matrix, axes='szyz'):
+        """Return Euler angles from rotation matrix for specified axis sequence.
+
+        axes : One of 24 axis sequences as string or encoded tuple
+
+        Note that many Euler angle triplets can describe one matrix."""
+        try:
+            firstaxis, parity, repetition, frame = RotationHandler._AXES2TUPLE[axes.lower()]
+        except (AttributeError, KeyError):
+            RotationHandler._TUPLE2AXES[axes]
+            firstaxis, parity, repetition, frame = axes
+
+        i = firstaxis
+        j = RotationHandler._NEXT_AXIS[i + parity]
+        k = RotationHandler._NEXT_AXIS[i - parity + 1]
+
+        M = np.array(matrix, dtype=np.float64, copy=False)[:3, :3]
+        if repetition:
+            sy = math.sqrt(M[i, j] * M[i, j] + M[i, k] * M[i, k])
+            if sy > RotationHandler._EPS:
+                ax = math.atan2(M[i, j], M[i, k])
+                ay = math.atan2(sy, M[i, i])
+                az = math.atan2(M[j, i], -M[k, i])
+            else:
+                ax = math.atan2(-M[j, k], M[j, j])
+                ay = math.atan2(sy, M[i, i])
+                az = 0.0
+        else:
+            cy = math.sqrt(M[i, i] * M[i, i] + M[j, i] * M[j, i])
+            if cy > RotationHandler._EPS:
+                ax = math.atan2(M[k, j], M[k, k])
+                ay = math.atan2(-M[k, i], cy)
+                az = math.atan2(M[j, i], M[i, i])
+            else:
+                ax = math.atan2(-M[j, k], M[j, j])
+                ay = math.atan2(-M[k, i], cy)
+                az = 0.0
+
+        if parity:
+            ax, ay, az = -ax, -ay, -az
+        if frame:
+            ax, az = az, ax
+        return -ax, -ay, -az
+
+    @staticmethod
+    def euler_from_quaternion(quaternion, axes='szyz'):
+        """Return Euler angles from quaternion for specified axis sequence."""
+        return euler_from_matrix(quaternion_matrix(quaternion), axes)
+
+    @staticmethod
+    def average_quaternions(quaternions):
+        """Calculates the average quaternion from a list of quaternions.
+        The input must be a np.array 2D matrix where each row is one quaternion"""
+        s = np.matmul(quaternions.T, quaternions)
+        s /= len(quaternions)
+        eigenValues, eigenVectors = np.linalg.eig(s)
+        return np.real(eigenVectors[:, np.argmax(eigenValues)])
+
+    @staticmethod
+    def maximum_distance(av_mat, mat):
+        """Max and argMax distance in angles from a set of rotation matrix"""
+        c = mat * av_mat[np.newaxis, 0:3, 0:3]
+        d = np.sum(c, axis=(1, 2))
+        ang_Distances = np.arccos((d - 1) / 2)
+        return np.max(ang_Distances), np.argmax(ang_Distances)
+
