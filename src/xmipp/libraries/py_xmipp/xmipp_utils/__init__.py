b946bb9430f5d54728a78ab4bec4fc1761e17f32
Carlos Oscar Sorzano <coss@cnb.csic.es>
2024-01-05 11:07:57 +0100

    XmippTrainingSequence added

--- src/xmipp/libraries/py_xmipp/xmipp_utils/__init__.py
+++ src/xmipp/libraries/py_xmipp/xmipp_utils/__init__.py
@@ -243,3 +243,23 @@ class RotationAverager():
             rot, tilt, psi = xmippLib.Euler_matrix2angles(RH.quaternion_matrix(avgQuaternion))
             avgAngles.append((rot,tilt,psi))
         return avgAngles
+
+try:
+    from keras.utils.all_utils import Sequence
+
+    class XmippTrainingSequence(Sequence):
+        def __init__(self, x_set, y_set, batch_size):
+            self.x, self.y = x_set, y_set
+            self.batch_size = batch_size
+
+        def __len__(self):
+            return int(np.ceil(len(self.x) / float(self.batch_size)))
+
+        def __getitem__(self, idx):
+            batch_x = self.x[idx * self.batch_size:(idx + 1) * self.batch_size]
+            batch_y = self.y[idx * self.batch_size:(idx + 1) * self.batch_size]
+
+            return batch_x, batch_y
+    KERAS_INSTALLED = True
+except:
+    KERAS_INSTALLED = False
