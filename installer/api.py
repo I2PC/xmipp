a3695a2d07a7e41e0d9129946ec2d5ac254a74d8
albertmena <12760268+albertmena@users.noreply.github.com>
2024-01-25 15:36:06 +0100

    mode all

--- installer/api.py
+++ installer/api.py
@@ -50,6 +50,7 @@ def sendApiPost(dictPackage: Dict, retCode: int=0):
 
 			# Send API POST request if there were no errors
 			if jsonStr is not None:
+				print(getCurlStr(API_URL, jsonStr))
 				runNetworkJob(getCurlStr(API_URL, jsonStr))
 	
 ####################### UTILS FUNCTIONS #######################
