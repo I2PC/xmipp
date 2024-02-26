98a45fad1671e7eca172b7ef3528552fbfd5df6a
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2024-02-26 14:27:48 +0000

    Fixing merge error

--- src/xmipp/libraries/py_xmipp/swiftalign/fourier/__init__.py
+++ src/xmipp/libraries/py_xmipp/swiftalign/fourier/__init__.py
@@ -20,29 +20,4 @@
 # *  e-mail address 'xmipp@cnb.csic.es'
 # ***************************************************************************/
 
-from typing import Optional, Sequence
-import torch
-
-def wiener_2d( direct_filter: torch.Tensor,
-               inverse_ssnr: Optional[torch.Tensor] = None,
-               out: Optional[torch.Tensor] = None) -> torch.Tensor:
-    
-    # Compute the filter power (|H|²) at the output
-    if torch.is_complex(direct_filter):
-        out = torch.abs(direct_filter, out=out)
-        out.square_()
-    else:
-        out = torch.square(direct_filter, out=out)
-    
-    # Compute the default value for inverse SSNR if not
-    # provided
-    if inverse_ssnr is None:
-        inverse_ssnr = torch.mean(out, dim=(-2, -1))
-        inverse_ssnr *= 0.1
-        inverse_ssnr = inverse_ssnr[...,None,None]
-    
-    # H* / (|H|² + N/S)
-    out.add_(inverse_ssnr)
-    torch.div(torch.conj(direct_filter), out, out=out)
-
-    return out
+from .rfftnfreq import rfftnfreq
