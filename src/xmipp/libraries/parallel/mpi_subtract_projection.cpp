14863200c0418c337ce5fbbbfd71ebcd18b31a91
Federico P. de Isidro-Gomez <federico.pdeisidro@astx.com>
2024-10-29 10:27:56 +0000

    fix uninitialization of mask projector when mask is not provided in mpi version

--- src/xmipp/libraries/parallel/mpi_subtract_projection.cpp
+++ src/xmipp/libraries/parallel/mpi_subtract_projection.cpp
@@ -52,8 +52,11 @@ void MpiProgSubtractProjection::preProcess()
     {
         realSize = (int)XSIZE(projector->VfourierRealCoefs);
         origin = STARTINGX(projector->VfourierRealCoefs);
-        realSizeMask = (int)XSIZE(projectorMask->VfourierRealCoefs);
-        originMask = STARTINGX(projectorMask->VfourierRealCoefs);
+        if (!fnMaskRoi.isEmpty())  // If there is provided mask
+		{
+            realSizeMask = (int)XSIZE(projectorMask->VfourierRealCoefs);
+            originMask = STARTINGX(projectorMask->VfourierRealCoefs);
+        }
     }
 
     MPI_Bcast(&realSize, 1, MPI_INT, 0, MPI_COMM_WORLD);
@@ -63,8 +66,11 @@ void MpiProgSubtractProjection::preProcess()
 
     MPI_Bcast(&realSizeMask, 1, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Bcast(&originMask, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Bcast(&(projectorMask->volumePaddedSize), 1, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Bcast(&projectorMask->volumeSize, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    if (!fnMaskRoi.isEmpty())  // If there is provided mask
+    {
+        MPI_Bcast(&(projectorMask->volumePaddedSize), 1, MPI_INT, 0, MPI_COMM_WORLD);
+        MPI_Bcast(&projectorMask->volumeSize, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    }
 
     if (rank != 0)
     {
@@ -73,21 +79,31 @@ void MpiProgSubtractProjection::preProcess()
         STARTINGX(projector->VfourierRealCoefs)=STARTINGY(projector->VfourierRealCoefs)=STARTINGZ(projector->VfourierRealCoefs)=origin;
         STARTINGX(projector->VfourierImagCoefs)=STARTINGY(projector->VfourierImagCoefs)=STARTINGZ(projector->VfourierImagCoefs)=origin;
 
-        projectorMask->VfourierRealCoefs.resizeNoCopy(realSizeMask,realSizeMask,realSizeMask);
-        projectorMask->VfourierImagCoefs.resizeNoCopy(realSizeMask,realSizeMask,realSizeMask);
-        STARTINGX(projectorMask->VfourierRealCoefs)=STARTINGY(projectorMask->VfourierRealCoefs)=STARTINGZ(projectorMask->VfourierRealCoefs)=originMask;
-        STARTINGX(projectorMask->VfourierImagCoefs)=STARTINGY(projectorMask->VfourierImagCoefs)=STARTINGZ(projectorMask->VfourierImagCoefs)=originMask;
+        if (!fnMaskRoi.isEmpty())  // If there is provided mask
+		{
+            projectorMask->VfourierRealCoefs.resizeNoCopy(realSizeMask,realSizeMask,realSizeMask);
+            projectorMask->VfourierImagCoefs.resizeNoCopy(realSizeMask,realSizeMask,realSizeMask);
+            STARTINGX(projectorMask->VfourierRealCoefs)=STARTINGY(projectorMask->VfourierRealCoefs)=STARTINGZ(projectorMask->VfourierRealCoefs)=originMask;
+            STARTINGX(projectorMask->VfourierImagCoefs)=STARTINGY(projectorMask->VfourierImagCoefs)=STARTINGZ(projectorMask->VfourierImagCoefs)=originMask;
+        }
     }
 
     MPI_Bcast(MULTIDIM_ARRAY(projector->VfourierRealCoefs), (int)MULTIDIM_SIZE(projector->VfourierRealCoefs), MPI_DOUBLE, 0, MPI_COMM_WORLD);
     MPI_Bcast(MULTIDIM_ARRAY(projector->VfourierImagCoefs), (int)MULTIDIM_SIZE(projector->VfourierImagCoefs), MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    MPI_Bcast(MULTIDIM_ARRAY(projectorMask->VfourierRealCoefs), (int)MULTIDIM_SIZE(projectorMask->VfourierRealCoefs), MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    MPI_Bcast(MULTIDIM_ARRAY(projectorMask->VfourierImagCoefs), (int)MULTIDIM_SIZE(projectorMask->VfourierImagCoefs), MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    if (!fnMaskRoi.isEmpty())  // If there is provided mask
+    {
+        MPI_Bcast(MULTIDIM_ARRAY(projectorMask->VfourierRealCoefs), (int)MULTIDIM_SIZE(projectorMask->VfourierRealCoefs), MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        MPI_Bcast(MULTIDIM_ARRAY(projectorMask->VfourierImagCoefs), (int)MULTIDIM_SIZE(projectorMask->VfourierImagCoefs), MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    }
 
     if (rank != 0)
     {
         projector->produceSideInfoProjection();
-        projectorMask->produceSideInfoProjection();
+        if (!fnMaskRoi.isEmpty())  // If there is provided mask
+		{
+            projectorMask->produceSideInfoProjection();
+        }
     }
 
     MetaData &mdIn = *getInputMd();
