d75700c2bc150a7d332e499128090afe9182c48d
albertmena <12760268+albertmena@users.noreply.github.com>
2024-12-04 12:29:54 +0100

    getModels!

--- installer/modelsDLTK.py
+++ installer/modelsDLTK.py
@@ -22,7 +22,7 @@
 
 import os, sys
 from .utils import runJob
-from .constants import SCIPION_SOFTWARE_EM, MODELS_URL
+from .constants import SCIPION_SOFTWARE_EM, MODELS_URL, DEFAULT_MODELS_DIR
 from .logger import blue, red, yellow, logger
 
 
@@ -43,10 +43,34 @@ def addModels(login: str, modelPath: str, update: bool):
 	
 	logger(f"Trying to upload the model using {login} as login",forceConsoleOutput=True)
 	args = "%s %s %s %s" % (login, os.path.abspath(localFileModel), SCIPION_SOFTWARE_EM, update)
-	retCode, log = runJob(f"dist/bin/xmipp_sync_data upload {args}", showCommand=True, showError=True)
+	retCode, log = runJob(f"dist/bin/xmipp_sync_data upload {args}", showCommand=False, showError=True)
 	if retCode == 0:
 		logger(f"{modelName} model successfully uploaded! Removing the local .tgz")
 		runJob("rm %s" % localFileModel)
 		
 		
-		
+
+def downloadDeepLearningModels(dest:str):
+    if not os.path.exists('dist/bin/xmipp_sync_data'):
+        logger(red('Xmipp has not been installed. Please, first install it '))
+        sys.exit()
+    if dest == DEFAULT_MODELS_DIR:
+        modelsPath = os.path.join(dest, 'models')
+    else:
+        modelsPath = dest
+    dataSet = "DLmodels"
+
+    # downloading/updating the DLmodels
+    if os.path.isdir(modelsPath):
+        logger("Updating the Deep Learning models (in backgound)")
+        task = "update"
+    else:
+        logger("Downloading Deep Learning models (in backgound)")
+        task = "download"
+    global pDLdownload
+
+    retCode, log = runJob(f"dist/bin/xmipp_sync_data {task} {modelsPath} {MODELS_URL} {dataSet}",
+           showCommand=True, showError=True, showOutput=True)
+    if retCode == 0:
+        logger(f"Models successfully {task}")
+
