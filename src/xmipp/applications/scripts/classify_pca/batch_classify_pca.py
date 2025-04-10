6761d3a9b5b8bacc33bf61f96e354e8b20410224
erneyramirez <erney.ramirez@gmail.com>
2025-04-10 11:41:34 +0200

    normalize particle

--- src/xmipp/applications/scripts/classify_pca/batch_classify_pca.py
+++ src/xmipp/applications/scripts/classify_pca/batch_classify_pca.py
@@ -279,6 +279,7 @@ if __name__=="__main__":
     # cl = bnb.normalize_particles_global(cl)
     # cl = bnb.process_images_iteratively(cl, 10)
     cl = bnb.center_by_com(cl)
+    cl = bnb.normalize_particles_global(cl)
 
     file_contrast = output+"_contrast.mrcs"
     save_images(cl.cpu().detach().numpy(), sampling, file_contrast)           
