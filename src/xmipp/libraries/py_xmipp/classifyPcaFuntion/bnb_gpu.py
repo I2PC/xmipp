77c7b8f932b1a9c193cbf6539ae9be3d70441b55
erneyramirez <erney.ramirez@gmail.com>
2025-04-15 13:03:13 +0200

    adaptative gaussian mask

--- src/xmipp/libraries/py_xmipp/classifyPcaFuntion/bnb_gpu.py
+++ src/xmipp/libraries/py_xmipp/classifyPcaFuntion/bnb_gpu.py
@@ -416,7 +416,10 @@ class BnBgpu:
         newCL = [torch.cat(class_images_list, dim=0) for class_images_list in newCL]    
         clk = self.averages_increaseClas(mmap, iter, newCL, classes)
         
-        clk = clk * self.create_circular_mask(clk)
+        if iter > 3 and iter < 13:
+            clk = clk * self.create_gaussian_masks_different_sigma(clk)
+        else:
+            clk = clk * self.create_circular_mask(clk)
         
         if iter < 3:
             clk = self.center_by_com(clk)     
