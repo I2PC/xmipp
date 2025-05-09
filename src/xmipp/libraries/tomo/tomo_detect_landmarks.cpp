46dd1a2ab0fd3a5e1b01baaea36958551452df47
Federico P. de Isidro-Gomez <federico.pdeisidro@astx.com>
2025-05-09 14:10:37 +0100

    remove unused libraries

--- src/xmipp/libraries/tomo/tomo_detect_landmarks.cpp
+++ src/xmipp/libraries/tomo/tomo_detect_landmarks.cpp
@@ -27,15 +27,7 @@
 #include <chrono>
 #include <core/metadata_label.h>
 #include <core/metadata_vec.h>
-#include <core/histogram.h>
-#include <data/mask.h>
 #include <fstream>
-#include <core/xmipp_image.h>
-#include <math.h>
-#include <limits>
-#include <complex>
-#include <string>
-#include <stdio.h>
 
 
 
