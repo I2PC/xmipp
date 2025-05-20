3d3fcb2c173f10674c54e8ef84c65e5f8395029a
erneyramirez <erney.ramirez@gmail.com>
2025-05-20 11:24:06 +0200

    automatic mask with otsu method

--- src/xmipp/libraries/py_xmipp/classifyPcaFuntion/bnb_gpu.py
+++ src/xmipp/libraries/py_xmipp/classifyPcaFuntion/bnb_gpu.py
@@ -420,7 +420,7 @@ class BnBgpu:
         newCL = [torch.cat(class_images_list, dim=0) for class_images_list in newCL]    
         clk = self.averages_increaseClas(mmap, iter, newCL, classes)
         
-        if iter > 3:
+        if iter > 3 and iter < 13:
             clk = clk * self.approximate_otsu_threshold(clk, percentile=30)
         clk = clk * self.create_circular_mask(clk)
         
