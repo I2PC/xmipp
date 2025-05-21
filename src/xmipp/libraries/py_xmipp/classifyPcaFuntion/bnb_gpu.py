47a747925c5e223afa1bff3367abd7869fed9e18
erneyramirez <erney.ramirez@gmail.com>
2025-05-21 11:56:52 +0200

    otsu 20 altern

--- src/xmipp/libraries/py_xmipp/classifyPcaFuntion/bnb_gpu.py
+++ src/xmipp/libraries/py_xmipp/classifyPcaFuntion/bnb_gpu.py
@@ -423,13 +423,13 @@ class BnBgpu:
         clk = self.averages_increaseClas(mmap, iter, newCL, classes)
         
         # if iter > 3 and iter < 13:
-        if iter in [3, 5, 7, 9, 11]:
+        if iter in [2, 4, 6, 8, 10]:
             clk = clk * self.approximate_otsu_threshold(clk, percentile=20)
 
             
         clk = clk * self.create_circular_mask(clk)
         
-        if iter < 3:
+        if iter in [2, 3]:
             clk = self.center_by_com(clk)     
         # if mask:
         #     if iter < 13:
