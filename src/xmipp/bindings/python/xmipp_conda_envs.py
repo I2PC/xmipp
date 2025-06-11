7b378e91c185d6b94707807f172141e612fa6f3f
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2025-06-11 11:35:55 +0200

    Added versioning

--- src/xmipp/bindings/python/xmipp_conda_envs.py
+++ src/xmipp/bindings/python/xmipp_conda_envs.py
@@ -5,27 +5,33 @@ _REQUIREMENT_PATH = os.path.join(os.path.dirname(__file__), 'envs_DLTK')
 XMIPP_CONDA_ENVS = {
   "xmipp_DLTK_v0.3": {
     "requirements": os.path.join(_REQUIREMENT_PATH, 'xmipp_DLTK_v0.3.yml'),
-    "xmippEnviron": True
+    "xmippEnviron": True,
+    "versionId": 1 # Increment when the corresponding yml is modified
   },
 
   "xmipp_MicCleaner": {
     "requirements": os.path.join(_REQUIREMENT_PATH, 'xmipp_MicCleaner.yml'),
-    "xmippEnviron": False
+    "xmippEnviron": False,
+    "versionId": 1 # Increment when the corresponding yml is modified
   },
 
   "xmipp_deepEMhancer": {
     "requirements": os.path.join(_REQUIREMENT_PATH, 'xmipp_deepEMhancer.yml'),
-    "xmippEnviron": False
+    "xmippEnviron": False,
+    "versionId": 1 # Increment when the corresponding yml is modified
   },
   
   "xmipp_pyTorch": {
     "requirements": os.path.join(_REQUIREMENT_PATH, 'xmipp_pyTorch.yml'),
-    "xmippEnviron": True
+    "xmippEnviron": True,
+    "versionId": 1 # Increment when the corresponding yml is modified
+
   },
 
   "xmipp_DLTK_v1.0": {
     "requirements": os.path.join(_REQUIREMENT_PATH, 'xmipp_DLTK_v1.0.yml'),
-    "xmippEnviron": True
+    "xmippEnviron": True,
+    "versionId": 1 # Increment when the corresponding yml is modified
   },
 
 }
