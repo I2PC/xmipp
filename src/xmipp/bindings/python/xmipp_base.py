8e529e71d1b70b42402cb4f2d2a22cdf62ba614e
alberto <alber.mena@gmail.com>
2025-06-13 11:53:08 +0200

    DLTK enviroments: limiting channels to conda-forge and pip (#1010)
    * tensorflow 1.15 to 2.6 DLTK_v0.3
    
    * xmipp_DLTK_v0.3 updated.
    Just conda-forge and pip channels
    
    * fixing bug in code hidden in tensorflow 1.15
    
    * enhances
    
    * enhances
    
    * enhances
    
    * enhances
    
    * enhances
    
    * enhances
    
    * enhances
    
    * enhances
    
    * enhances
    
    * enhances
    
    * enhances
    
    * back to tensorflow 1.15
    
    * back to tensorflow 1.15
    
    * back to tensorflow 1.15
    
    * back to tensorflow 1.15
    
    * Update deepConsensus_networkDef.py
    
    * just conda-forge and pip channels
    
    * just conda-forge and pip channels
    
    * just conda-forge and pip channels
    
    * just conda-forge and pip channels
    
    * just conda-forge and pip channels
    
    * just conda-forge and pip channels
    
    * just conda-forge and pip channels
    
    * just conda-forge and pip channels
    
    * just conda-forge and pip channels
    
    * just conda-forge and pip channels
    
    * just conda-forge and pip channels
    
    * removing xmipp_graph env
    
    * removing xmipp_graph env
    
    * Update xmipp_DLTK_v1.0-gpu.yml
    
    * Update xmipp_MicCleaner.yml
    
    * Update xmipp_MicCleaner.yml
    
    * Update xmipp_MicCleaner.yml
    
    * emhancer upload version to tensorflow 2.10
    
    * Update xmipp_deepEMhancer.yml
    
    * Update xmipp_deepEMhancer.yml
    
    * minors
    
    * minors
    
    * changes in pytorch env
    
    * including   - libstdcxx-ng=14.2.0 # To be compatible with scipion env
    
    * Installing all pytorch in conda-forge
    
    * Update xmipp_pyTorch-gpu.yml
    
    * note
    
    * update model deepEMhancr
    
    * no version on libstdcxx-ng
    
    * model version for deepEMhancer
    
    * Added versioning
    
    * Empty line
    
    * numpy with pip
    
    * simplifying env installation
    
    * no version for libstdcxx-ng
    
    * enhancing the solving
    
    * enhancing the solving
    
    * fixing the version of deepEMhancer
    
    * version libstdcxx-ng
    
    ---------
    
    Co-authored-by: Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>

--- src/xmipp/bindings/python/xmipp_base.py
+++ src/xmipp/bindings/python/xmipp_base.py
@@ -286,7 +286,12 @@ class CondaEnvManager(object):
     @staticmethod
     def yieldInstallAllCmds(useGpu):
         for name, env in CondaEnvManager.XMIPP_CONDA_ENVS.items():
-            yield CondaEnvManager.installEnvironCmd(name, env['requirements'], gpu=useGpu)
+            yield CondaEnvManager.installEnvironCmd(
+                name=name, 
+                versionId=env.get('versionId', None), 
+                requirementsFn=env['requirements'], 
+                gpu=useGpu
+            )
 
     @staticmethod
     def getCurInstalledDep(dependency, defaultVersion=None, environ=None):
@@ -309,7 +314,7 @@ class CondaEnvManager(object):
         return dependency+'=='+defaultVersion if defaultVersion else dependency
 
     @staticmethod
-    def installEnvironCmd(name: str, requirementsFn: str, gpu=False):
+    def installEnvironCmd(name: str,requirementsFn: str,  versionId: int = None, gpu=False):
         # Consider the gpu version if requested
         if gpu:
             root, ext = os.path.splitext(requirementsFn)
@@ -318,7 +323,11 @@ class CondaEnvManager(object):
             if os.path.exists(gpuRequirementsFn):
                 requirementsFn = gpuRequirementsFn
         
-        target = name + '.yml'
+        if versionId is not None:
+            target = f'{name}-{versionId}.yml'
+        else:
+            target = f'{name}.yml'
+
         commands = [] 
         commands.append('conda env create -f %s || conda env update -f %s' % (requirementsFn, requirementsFn))
         commands.append('conda env export -f %s' % target)
