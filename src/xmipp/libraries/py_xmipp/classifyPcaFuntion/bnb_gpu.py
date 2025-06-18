7b8ee2a54f8a0ad59f0796632eef2909aecd2689
erneyramirez <erney.ramirez@gmail.com>
2025-06-18 13:20:21 +0200

    multiresolution, filter butterworth, include PCA, no sharp

--- src/xmipp/libraries/py_xmipp/classifyPcaFuntion/bnb_gpu.py
+++ src/xmipp/libraries/py_xmipp/classifyPcaFuntion/bnb_gpu.py
@@ -1340,10 +1340,10 @@ class BnBgpu:
     def enhance_averages_butterworth(self, 
         averages,
         sampling=1.5,
-        low_res_angstrom=15.0,
+        low_res_angstrom=20.0,
         high_res_angstrom=6.0,
         order=4,
-        blend_factor=0.4,
+        blend_factor=0.5,
         normalize=True
     ):
         """
