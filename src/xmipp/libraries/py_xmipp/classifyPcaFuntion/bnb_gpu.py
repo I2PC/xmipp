ac12082ecdccc78a174bd4e91990a41596270737
erneyramirez <erney.ramirez@gmail.com>
2025-06-20 17:23:03 +0200

    test drástico

--- src/xmipp/libraries/py_xmipp/classifyPcaFuntion/bnb_gpu.py
+++ src/xmipp/libraries/py_xmipp/classifyPcaFuntion/bnb_gpu.py
@@ -1346,8 +1346,8 @@ class BnBgpu:
     def enhance_averages_butterworth(self, 
         averages,
         sampling=1.5,
-        low_res_angstrom=20.0,
-        high_res_angstrom=4.0,
+        low_res_angstrom=30.0,
+        high_res_angstrom=6.0,
         order=4,
         blend_factor=0.5,
         normalize=True
