e23bf197f62c575be37265e43008cf1c21adabae
erneyramirez <erney.ramirez@gmail.com>
2025-06-27 10:04:24 +0200

    PCA inside, thr=2, resolution for classes, first filter adaptive, butterworthAdapt hasta 25, multiresolution

--- src/xmipp/libraries/py_xmipp/classifyPcaFuntion/bnb_gpu.py
+++ src/xmipp/libraries/py_xmipp/classifyPcaFuntion/bnb_gpu.py
@@ -513,10 +513,10 @@ class BnBgpu:
         if iter > 10: 
             res_classes = self.frc_resolution_tensor(newCL, sampling)
             print(res_classes) 
+            clk = self.gaussian_lowpass_filter_2D_adaptive(clk, res_classes, sampling)
             clk = self.enhance_averages_butterworth_adaptive(clk, res_classes, sampling)
             # clk = self.unsharp_mask_norm(clk)
             # clk = self.enhance_averages_butterworth(clk, sampling)
-            clk = self.gaussian_lowpass_filter_2D_adaptive(clk, res_classes, sampling)
 
             # clk = self.enhance_averages_butterworth(clk, sampling=sampling) 
         #     clk = self.unsharp_mask_norm(clk) 
@@ -691,10 +691,10 @@ class BnBgpu:
             
             # clk = self.unsharp_mask_norm(clk) 
             res_classes = self.frc_resolution_tensor(newCL, sampling)
+            clk = self.gaussian_lowpass_filter_2D_adaptive(clk, res_classes, sampling)
             clk = self.enhance_averages_butterworth_adaptive(clk, res_classes, sampling)
             # clk = self.enhance_averages_butterworth(clk, sampling)
             # clk = self.gaussian_lowpass_filter_2D(clk, maxRes, sampling)
-            clk = self.gaussian_lowpass_filter_2D_adaptive(clk, res_classes, sampling)
         
             
             # clk = self.unsharp_mask_adaptive_gaussian(clk)
@@ -1588,12 +1588,9 @@ class BnBgpu:
         # === 4. Frecuencias normalizadas respecto al Nyquist (∈ [0, 0.5])
         f_low = (1.0 / low_res) / nyquist / 2.0
         f_high = (1.0 / high_res) / nyquist / 2.0
-        print(f_high)
         MAX_CUTOFF = 0.475
         f_low  = torch.clamp(f_low,  min=0.0, max=MAX_CUTOFF)
         f_high = torch.clamp(f_high, min=0.0, max=MAX_CUTOFF)
-        print("---------------")
-        print(f_high)
         f_low = f_low.view(B, 1, 1)
         f_high = f_high.view(B, 1, 1)
     
