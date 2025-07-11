cb5763c078c41dd9fe5a64e1981246e2dc509f58
erneyramirez <erney.ramirez@gmail.com>
2025-07-11 14:24:09 +0200

    18 iter, thr 2.0, butterAttenuate (15_order 4) (include filter, multiresol

--- src/xmipp/libraries/py_xmipp/classifyPcaFuntion/bnb_gpu.py
+++ src/xmipp/libraries/py_xmipp/classifyPcaFuntion/bnb_gpu.py
@@ -1636,7 +1636,7 @@ class BnBgpu:
         resolutions: torch.Tensor,         # [B] resoluciones FRC por clase
         pixel_size: float,                 # Å/pixel
         low_res_angstrom: float = 20.0,    # corte para altas frecuencias
-        order: int = 2,                    # orden del filtro
+        order: int = 4,                    # orden del filtro
         blend_factor: float = 0.5,         # mezcla con original
         normalize: bool = True             # conservar contraste
     ) -> torch.Tensor:
@@ -1714,7 +1714,7 @@ class BnBgpu:
         averages: torch.Tensor,            # [B, H, W]
         resolutions: torch.Tensor,         # [B] resoluciones FRC por clase
         pixel_size: float,                 # Å/pixel
-        low_res_angstrom: float = 20.0,    # corte para bajas frecuencias a atenuar
+        low_res_angstrom: float = 15.0,    # corte para bajas frecuencias a atenuar
         order: int = 4,                    # orden del filtro
         blend_factor: float = 0.5,         # mezcla con original
         normalize: bool = True             # conservar contraste
